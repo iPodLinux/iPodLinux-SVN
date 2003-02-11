5b7baa077414d2a9c9779ad364a93d18e566e209
nobody <nobody@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2003-02-11 12:22:49 +0000

    This commit was manufactured by cvs2svn to create tag 'IPOD_LINUX_0_1'.

---
+++ tags/IPOD_LINUX_0_1/linux/include/asm-armnommu/arch-ipod/hardware.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2003, Bernard Leach (leachbj@bouncycastle.org)
+ */
+
+#ifndef __ASM_ARCH_HARDWARE_H
+#define __ASM_ARCH_HARDWARE_H
+
+/* this is called from drivers/block/blkmem.c */
+#define HARD_RESET_NOW() ipod_hard_reset()
+
+#endif
+
