69d42dcbfe30be20bdd8fbdd31f06beaf3120ee4
leachbj <leachbj@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2003-02-17 17:30:25 +0000

    added scroll whell support fixed hold switch -> now sends ^S and ^Q combinations menu/play pause now change the contrast setting of the LCD now uses shared irq to allow for other GPIO interrupt handlers

--- trunk/linux/arch/armnommu/mach-ipod/keyboard.c
+++ trunk/linux/arch/armnommu/mach-ipod/keyboard.c
@@ -17,21 +17,10 @@
 #include <asm/arch/irqs.h>
 #include <asm/keyboard.h>
 
-static void keyboard_interrupt(int irq, void *dev_id, struct pt_regs *regs)
-{
-#if 1
-	static int left = 0, scrolling = 0;
-#endif
-	unsigned char source, state;
-
-	/* get source of interupts */
-	source = inb(0xcf000040);
-
-	/* get current keypad status */
-	state = inb(0xcf000030);
-	outb(~state, 0xcf000060);
-
 /* we use the keycodes and translation is 1 to 1 */
+#define R_SC		0x13
+#define L_SC		0x26
+
 #define UP_SC		103
 #define LEFT_SC		105
 #define RIGHT_SC	106
@@ -39,16 +28,39 @@ static void keyboard_interrupt(int irq, void *dev_id, struct pt_regs *regs)
 
 #define ACTION_SC	28
 
+/* send ^S and ^Q for the hold switch */
+#define LEFT_CTRL_SC	29
+#define Q_SC		16
+#define S_SC		31
+
+/* need to pass something becuase we use a shared irq */
+#define KEYBOARD_DEV_ID	0x4b455942
+
+static void keyboard_interrupt(int irq, void *dev_id, struct pt_regs *regs)
+{
+	static int prev_scroll = -1;
+	unsigned char source, state;
+
+	/* get source of interupts */
+	source = inb(0xcf000040);
+	if ( source == 0 ) {
+		return; 	/* not for us */
+	}
+
+	/* get current keypad status */
+	state = inb(0xcf000030);
+	outb(~state, 0xcf000060);
+
 #ifdef CONFIG_VT
 	kbd_pt_regs = regs;
 
 	if ( source & 0x1 ) {
 		if ( state & 0x1 ) {
-#if 0
 			//printk("ff up\n");
+#if 0
 			handle_scancode(RIGHT_SC, 0);
 #else
-scrollfront(0);
+			scrollfront(0);
 #endif
 		}
 		else {
@@ -69,105 +81,111 @@ scrollfront(0);
 		}
 	}
 
-#define LEFT_CTRL_SC	29
-#define Q_SC		16
-#define S_SC		31
-
 	if ( source & 0x4 ) {
 		if ( state & 0x4 ) {
-#if 0
 			//printk("pause up\n");
+#if 0
 			handle_scancode(DOWN_SC, 0);
 #else
-			// simulate CTRL-Q up
-			handle_scancode(Q_SC, 0);
-			handle_scancode(LEFT_CTRL_SC, 0);
+			contrast_down();
 #endif
 		}
 		else {
-#if 0
 			//printk("pause down\n");
+#if 0
 			handle_scancode(DOWN_SC, 1);
-#else
-			// simulate CTRL-Q down
-			handle_scancode(LEFT_CTRL_SC, 1);
-			handle_scancode(Q_SC, 1);
 #endif
 		}
 	}
 	if ( source & 0x8 ) {
 		if ( state & 0x8 ) {
-#if 0
 			//printk("rr up\n");
+#if 0
 			handle_scancode(LEFT_SC, 0);
 #else
-scrollback(0);
+			scrollback(0);
 #endif
 		}
 		else {
-#if 0
 			//printk("rr down\n");
+#if 0
 			handle_scancode(LEFT_SC, 1);
 #endif
 		}
 	}
 	if ( source & 0x10 ) {
 		if ( state & 0x10 ) {
-#if 0
 			//printk("menu up\n");
+#if 0
 			handle_scancode(LEFT_SC, 0);
 #else
-			/* CTRL-S up */
-			handle_scancode(S_SC, 0);
-			handle_scancode(LEFT_CTRL_SC, 0);
+			contrast_up();
 #endif
 		}
 		else {
-#if 0
 			//printk("menu down\n");
+#if 0
 			handle_scancode(LEFT_SC, 1);
-#else
-			/* CTRL-S down */
-			handle_scancode(LEFT_CTRL_SC, 1);
-			handle_scancode(S_SC, 1);
 #endif
 		}
 	}
 	if ( source & 0x20 ) {
 		if ( state & 0x20 ) {
-			//printk("menu up\n");
-			handle_scancode(UP_SC, 0);
+			//printk("hold on\n");
+			/* CTRL-S down */
+			handle_scancode(LEFT_CTRL_SC, 1);
+			handle_scancode(S_SC, 1);
+
+			/* CTRL-S up */
+			handle_scancode(S_SC, 0);
+			handle_scancode(LEFT_CTRL_SC, 0);
 		}
 		else {
-			//printk("menu down\n");
-			handle_scancode(UP_SC, 1);
+			//printk("hold off\n");
+			/* CTRL-Q down */
+			handle_scancode(LEFT_CTRL_SC, 1);
+			handle_scancode(Q_SC, 1);
+
+			/* CTRL-Q up */
+			handle_scancode(Q_SC, 0);
+			handle_scancode(LEFT_CTRL_SC, 0);
 		}
 	}
-#endif
 
-#if 0
-	if ( (source & 0xc0) == 0xc0 ) {
-		scrolling = 1;
-		if ( left ) {
-			printk("left\n");
+	if ( source & 0xc0 ) {
+		static int scroll_state[4][4] = {
+			{0, 1, -1, 0},
+			{-1, 0, 0, 1},
+			{1, 0, 0, -1},
+			{0, -1, 1, 0}
+		};
+		unsigned now_scroll = (state & 0xc0) >> 6;
+						
+		if ( prev_scroll == -1 ) {
+			prev_scroll = now_scroll;
 		}
 		else {
-			printk("right\n");
+			switch (scroll_state[prev_scroll][now_scroll]) {
+			case 1:
+				/* 'l' keypress */
+				handle_scancode(L_SC, 1);
+				handle_scancode(L_SC, 0);
+				break;
+			case -1:
+				/* 'r' keypress */
+				handle_scancode(R_SC, 1);
+				handle_scancode(R_SC, 0);
+				break;
+			default:
+				/* only happens if we get out of sync */
+			}
 		}
-	}
-	else if ( source & 0x40 ) {
-		left = 1;
-		printk("left\n");
-	}
-	else if ( source & 0x80 ) {
-		left = 0;
-		printk("right\n");
-	}
-#endif
 
-#ifdef CONFIG_VT
+		prev_scroll = now_scroll;
+	}
+
 	tasklet_schedule(&keyboard_tasklet);
-#endif
+#endif /* CONFIG_VT */
 
 	/* ack any active interrupts */
 	outb(source, 0xcf000070);
@@ -182,7 +200,7 @@ void __init ipodkb_init_hw(void)
 	outb(inb(0xcf000014) | 0x1, 0xcf000014);
 	outb(inb(0xcf000024) | 0x1, 0xcf000024);
 
-	if ( request_irq(GPIO_IRQ, keyboard_interrupt, 0, "keyboard", NULL) ) {
+	if ( request_irq(GPIO_IRQ, keyboard_interrupt, SA_SHIRQ, "keyboard", KEYBOARD_DEV_ID) ) {
 		printk("ipodkb: IRQ %d failed\n", GPIO_IRQ);
 	}
 
