d43ec3abb042a58345f9ba726810fa2c0a1dd9e7
nobody <nobody@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2003-03-12 13:26:51 +0000

    This commit was manufactured by cvs2svn to create tag 'IPOD_LINUX_0_2'.

---
+++ tags/IPOD_LINUX_0_2/linux/include/asm-armnommu/arch-ipod/dma.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2003, Bernard Leach (leachbj@bouncycastle.org)
+ */
+
+#ifndef __ASM_ARCH_DMA_H
+#define __ASM_ARCH_DMA_H
+
+/* we don't have any DMA */
+#define MAX_DMA_ADDRESS		0xffffffff
+#define MAX_DMA_CHANNELS	0
+
+#endif
+
