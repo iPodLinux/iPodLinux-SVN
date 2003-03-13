720f49b24d139b3d7f2e2b7d555e8127ec1eee09
leachbj <leachbj@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2003-03-13 20:22:46 +0000

    added support for the two serial interrupts

--- trunk/linux/include/asm-armnommu/arch-ipod/irqs.h
+++ trunk/linux/include/asm-armnommu/arch-ipod/irqs.h
@@ -6,13 +6,15 @@
 #define __ASM_ARCH_IRQS_H__
 
 #define IDE_INT0_IRQ	1
+#define SER0_IRQ	4
+#define SER1_IRQ	7
 #define TIMER1_IRQ	11
 #define GPIO_IRQ	14
 #define DMA_OUT_IRQ	30
 
 #define NR_IRQS	32
 
-#define VALID_IRQ(x)	(x==IDE_INT0_IRQ||x==TIMER1_IRQ||x==GPIO_IRQ||x==DMA_OUT_IRQ)
+#define VALID_IRQ(x)	(x==IDE_INT0_IRQ||x==SER0_IRQ||x==SER1_IRQ||x==TIMER1_IRQ||x==GPIO_IRQ||x==DMA_OUT_IRQ)
 
 #endif
 
