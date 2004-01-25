f8aa2a9216d2150d85b61bf74e0a4aa060bd64cd
leachbj <leachbj@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2004-01-25 11:29:12 +0000

    Import of uClinux-2.4.24-uc0

--- branches/uClinux/linux/include/asm-armnommu/pci.h
+++ branches/uClinux/linux/include/asm-armnommu/pci.h
@@ -18,7 +18,15 @@ extern inline void pcibios_penalize_isa_irq(int irq)
 #include <asm/scatterlist.h>
 #include <asm/io.h>
 
+
 struct pci_dev;
+
+/*
+ * The PCI address space does equal the physical memory address space.
+ * The networking and block device layers use this boolean for bounce
+ * buffer decisions.
+ */
+#define PCI_DMA_BUS_IS_PHYS     (0)
 
 /* Allocate and map kernel buffer using consistent mode DMA for a device.
  * hwdev should be valid struct pci_dev pointer for PCI devices,
