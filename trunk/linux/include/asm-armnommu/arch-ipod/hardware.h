dc1bf6ee45ebc065d14f0560c4e9c3028dd4c975
leachbj <leachbj@bouncycastle.org>
2004-02-12 10:22:20 +0000

    first cut at getting SysI from bootloader

--- trunk/linux/include/asm-armnommu/arch-ipod/hardware.h
+++ trunk/linux/include/asm-armnommu/arch-ipod/hardware.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, Bernard Leach (leachbj@bouncycastle.org)
+ * Copyright (c) 2003,2004 Bernard Leach (leachbj@bouncycastle.org)
  */
 
 #ifndef __ASM_ARCH_HARDWARE_H
@@ -7,6 +7,17 @@
 
 /* this is called from drivers/block/blkmem.c */
 #define HARD_RESET_NOW() ipod_hard_reset()
+
+#ifndef __ASSEMBLY__
+struct sysinfo_t {
+	unsigned IsyS;
+	unsigned pad[17];
+	unsigned boardHwSwInterfaceRev; /* #72 */
+};
+
+extern unsigned ipod_get_hw_version(void);
+extern struct sysinfo_t *ipod_get_sysinfo(void);
+#endif
 
 #endif
 
