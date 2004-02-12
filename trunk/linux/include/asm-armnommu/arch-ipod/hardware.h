d31f5687e267494e4502136c727da2e91a22cbe7
leachbj <leachbj@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2004-02-12 21:04:42 +0000

    filled in correct values for sysinfo_t

--- trunk/linux/include/asm-armnommu/arch-ipod/hardware.h
+++ trunk/linux/include/asm-armnommu/arch-ipod/hardware.h
@@ -10,9 +10,57 @@
 
 #ifndef __ASSEMBLY__
 struct sysinfo_t {
-	unsigned IsyS;
-	unsigned pad[17];
-	unsigned boardHwSwInterfaceRev; /* #72 */
+	unsigned IsyS;  /* == "IsyS" */
+	unsigned len;
+
+	char BoardHwName[16];
+	char pszSerialNumber[32];
+	char pu8FirewireGuid[16];
+
+	unsigned boardHwRev;
+	unsigned bootLoaderImageRev;
+	unsigned diskModeImageRev;
+	unsigned diagImageRev;
+	unsigned osImageRev;
+
+	unsigned iram_perhaps;
+
+	unsigned Flsh;
+	unsigned flash_zero;
+	unsigned flash_base;
+	unsigned flash_size;
+	unsigned flash_zero2;
+								
+	unsigned Sdrm;
+	unsigned sdram_zero;
+	unsigned sdram_base;
+	unsigned sdram_size;
+	unsigned sdram_zero2;
+								
+	unsigned Frwr;
+	unsigned frwr_zero;
+	unsigned frwr_base;
+	unsigned frwr_size;
+	unsigned frwr_zero2;
+								
+	unsigned Iram;
+	unsigned iram_zero;
+	unsigned iram_base;
+	unsigned iram_size;
+	unsigned irame_zero2;
+								
+	char pad7[120];
+
+	unsigned boardHwSwInterfaceRev;
+
+	/* added in V3 */
+	char HddFirmwareRev[10];
+
+	unsigned short RegionCode;
+
+	unsigned PolicyFlags;
+
+	char ModelNumStr[16];
 };
 
 extern unsigned ipod_get_hw_version(void);
