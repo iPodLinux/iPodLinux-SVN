6eeae6d684d7e4346c7d45851f87e5b98318a169
leachbj <leachbj@bouncycastle.org>
2004-05-17 18:18:04 +0000

    prototype for ipod_i2c_init

--- trunk/linux/include/asm-armnommu/arch-ipod/hardware.h
+++ trunk/linux/include/asm-armnommu/arch-ipod/hardware.h
@@ -65,8 +65,11 @@ struct sysinfo_t {
 
 extern unsigned ipod_get_hw_version(void);
 extern struct sysinfo_t *ipod_get_sysinfo(void);
+
+extern void ipod_i2c_init(void);
 extern int ipod_i2c_send_bytes(unsigned int addr, unsigned int len, unsigned char *data);
 extern int ipod_i2c_send(unsigned int addr, int data0, int data1);
+
 extern void ipod_serial_init(void);
 
 typedef void (*ipod_dma_handler_t)(void);
