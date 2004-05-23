2c12df3d9894c59c91271ed826803f26d0b7bc8c
leachbj <leachbj@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2004-05-23 14:51:06 +0000

    added dma in irq

--- trunk/linux/include/asm-armnommu/arch-ipod/irqs.h
+++ trunk/linux/include/asm-armnommu/arch-ipod/irqs.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, Bernard Leach (leachbj@bouncycastle.org)
+ * Copyright (c) 2003, Bernard Leach <leachbj@bouncycastle.org>
  */
 
 #ifndef __ASM_ARCH_IRQS_H__
@@ -12,10 +12,11 @@
 #define TIMER1_IRQ	11
 #define GPIO_IRQ	14
 #define DMA_OUT_IRQ	30
+#define DMA_IN_IRQ	31
 
 #define NR_IRQS	32
 
-#define VALID_IRQ(x)	(x==IDE_INT0_IRQ||x==SER0_IRQ||x==I2S_IRQ||x==SER1_IRQ||x==TIMER1_IRQ||x==GPIO_IRQ||x==DMA_OUT_IRQ)
+#define VALID_IRQ(x)	(x==IDE_INT0_IRQ||x==SER0_IRQ||x==I2S_IRQ||x==SER1_IRQ||x==TIMER1_IRQ||x==GPIO_IRQ||x==DMA_OUT_IRQ||x==DMA_IN_IRQ)
 
 #endif
 
