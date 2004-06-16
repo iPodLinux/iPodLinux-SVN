a87b89dc686966987b7d2bd83f7ef99ee15420c8
nobody <nobody@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2004-06-16 23:14:07 +0000

    This commit was manufactured by cvs2svn to create tag 'bluecube_0_9'.

---
+++ tags/bluecube_0_9/tools/podzilla/bluecube/pieces.h
@@ -0,0 +1,29 @@
+/*
+ * BlueCube - just another tetris clone 
+ * Copyright (C) 2003  Sebastian Falbesoner
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+/* File: pieces.h === To make the pieces and the colortable available 
+                      in other sourcefiles */
+
+#ifndef __PIECES_H__
+#define __PIECES_H__
+
+extern int StyleColors[][3];
+extern int Pieces[][4][CLUSTER_X][CLUSTER_Y];
+
+#endif
