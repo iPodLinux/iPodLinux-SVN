5cca87cf9daee53a659e09fa5696118e4623c93e
leachbj <leachbj@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2004-09-03 17:33:38 +0000

    added constants for timer0

--- trunk/linux/include/asm-armnommu/arch-ipod/timex.h
+++ trunk/linux/include/asm-armnommu/arch-ipod/timex.h
@@ -5,5 +5,11 @@
 #ifndef __ASM_ARCH_TIMEX_H__
 #define __ASM_ARCH_TIMEX_H__
 
+#define IPOD_TIMER0       0xcf001100
+#define IPOD_TIMER0_ACK   0xcf001104
+#define IPOD_TIMER_STATUS 0xcf001110
+
+#define USECS_PER_INT 0x2710
+
 #endif
 
