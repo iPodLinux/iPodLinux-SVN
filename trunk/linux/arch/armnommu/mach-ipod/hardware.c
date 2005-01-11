ef3a64d111f6b3665361d255574cd81451180441
leachbj <leachbj@bouncycastle.org>
2005-01-11 10:16:20 +0000

    moved dma function pointer to fast ram removed some unused code

--- trunk/linux/arch/armnommu/mach-ipod/hardware.c
+++ trunk/linux/arch/armnommu/mach-ipod/hardware.c
@@ -18,6 +18,9 @@
 #define SYSINFO_TAG     (unsigned char *)0x40017f18
 #define SYSINFO_PTR     (struct sysinfo_t **)0x40017f1c
 
+/* this must match the value in audio.c */
+#define DMA_HANDLER	(ipod_dma_handler_t *)0x40000010
+
 static struct sysinfo_t ipod_sys_info;
 static int ipod_sys_info_set;
 
@@ -75,46 +78,6 @@ void ipod_hard_reset(void)
 	outl(inl(0xcf005030) | 0x4, 0xcf005030);
 }
 
-/* make startup piezo noise */
-void ipod_startup_noise(void)
-{
-	int i1, i2;
-
-	outl(0x80, 0xc000604c);
-
-	/* set divisor low word */
-	outl(0xd, 0xc0006040);
-
-	outl(0x3, 0xc000604c);
-
-	outl(0x3, 0xc0006050);
-	outl(0x7, 0xc0006048);
-
-	for ( i1 = 0; i1 < 500; i1++ ) {
-		outl(0x0, 0xc0006040);
-
-		/* delay for a bit */
-		for ( i2 = 0; i2 < i1; i2++ ) {
-			/* empty */
-		}
-	}
-}
-
-/* wait for action button to be pressed and then released */
-void
-wait_for_action(void)
-{
-	/* wait for press */
-	do {
-		inl(0xcf000030);
-	} while ( (inl(0xcf000030) & 0x2) != 0 );
-
-	/* wait for release */
-	do {
-		inl(0xcf000030);
-	} while ( (inl(0xcf000030) & 0x2) == 0 );
-}
-
 void
 ipod_init_cache(void)
 {
@@ -161,69 +124,6 @@ ipod_set_cpu_speed(void)
 	udelay(2000);
 
 	outl(0xa8, 0xcf00500c);
-}
-
-static void
-set_boot_variable(unsigned *dest, char *src, unsigned len)
-{
-	if ( !(((unsigned)dest | (unsigned)src) & 0x3 ) ) {
-
-		unsigned lr = 0x01010101;
-
-		unsigned ip;
-		unsigned t;
-
-		do {
-			if ( len < 4 ) break;
-
-			ip = *(unsigned *)src;
-			*dest = ip;
-
-			dest ++;
-			src += 4;
-			len -= 4;
-
-			/* this magic checks for embedded nulls */
-			t = ip - lr;
-			t = t & ~ip;
-		} while ( (t & (lr << 7)) == 0 );
-	}
-
-	if ( len == 0 ) return;
-
-	{
-		char ip;
-
-		do {
-			ip = *src++;
-			*((char *)dest)++ = ip;
-			if ( ip == 0 ) break;
-			len--;
-		} while ( len != 0 );
-
-		if ( len <= 1 ) return;
-	}
-
-	len --;
-
-	do {
-		*((unsigned char *)dest)++ = '\0';
-		len--;
-	} while ( len != 0 );
-}
-
-void
-ipod_reboot_to_diskmode(void)
-{
-	/* select new mode and icon to show during boot */
-	set_boot_variable((unsigned *)0x40017f00, "diskmode", 8);
-	set_boot_variable((unsigned *)0x40017f08, "hotstuff", 8);
-
-	/* do special startup? */
-	outl(0x1, 0x40017f10);
-
-	/* reset */
-	outl(inl(0xcf005030) | 0x4, 0xcf005030);
 }
 
 #define IPOD_I2C_CTRL	0xc0008000
@@ -407,7 +307,7 @@ ipod_serial_init(void)
 }
 
 /* put our ptr in on chip ram to avoid caching problems */
-static ipod_dma_handler_t * ipod_dma_handler = SYSINFO_TAG;
+static ipod_dma_handler_t * ipod_dma_handler = DMA_HANDLER;
 
 void ipod_set_process_dma(ipod_dma_handler_t new_handler)
 {
