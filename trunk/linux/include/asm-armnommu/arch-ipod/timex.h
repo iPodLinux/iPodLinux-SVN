257ae487dc9c9a45717b3de1ca42b58cc8bf5532
leachbj <leachbj@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2005-02-01 14:51:44 +0000

    supoprt for PP5020 timer

--- trunk/linux/include/asm-armnommu/arch-ipod/timex.h
+++ trunk/linux/include/asm-armnommu/arch-ipod/timex.h
@@ -1,15 +1,26 @@
 /*
- * Copyright (c) 2003, Bernard Leach (leachbj@bouncycastle.org)
+ * Copyright (c) 2003-2005, Bernard Leach (leachbj@bouncycastle.org)
  */
 
 #ifndef __ASM_ARCH_TIMEX_H__
 #define __ASM_ARCH_TIMEX_H__
 
-#define IPOD_TIMER0       0xcf001100
-#define IPOD_TIMER0_ACK   0xcf001104
-#define IPOD_TIMER_STATUS 0xcf001110
-
 #define USECS_PER_INT 0x2710
+
+/* PP5002 registers */
+#define PP5002_TIMER1       0xcf001100
+#define PP5002_TIMER1_ACK   0xcf001104
+
+#define PP5002_TIMER_STATUS 0xcf001110
+
+/* PP5020 registers */
+#define PP5020_TIMER1       0x60005000
+#define PP5020_TIMER1_ACK   0x60005004
+
+#define PP5020_TIMER2       0x60005008
+#define PP5020_TIMER2_ACK   0x6000500c
+
+#define PP5020_TIMER_STATUS 0x60005010
 
 #endif
 
