788ffa150fae2dd68f4bfc35c7d793740949f22f
leachbj <leachbj@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2005-02-01 14:51:01 +0000

    ipod_gettimeoffset support for PP5020

--- trunk/linux/arch/armnommu/mach-ipod/time.c
+++ trunk/linux/arch/armnommu/mach-ipod/time.c
@@ -1,7 +1,7 @@
 /*
  * time.c - timer support for iPod
  *
- * Copyright (c) 2003, Bernard Leach (leachbj@bouncycastle.org)
+ * Copyright (c) 2003-2005, Bernard Leach (leachbj@bouncycastle.org)
  */
 
 #include <linux/config.h>
@@ -25,7 +25,16 @@ extern int ipod_set_rtc(void)
  */
 extern unsigned long ipod_gettimeoffset(void)
 {
-	return USECS_PER_INT - (inl(IPOD_TIMER0) & 0xffff);
+	unsigned int diff;
+
+	if ((ipod_get_hw_version() >> 16) > 0x3) {
+		diff = inl(PP5020_TIMER1);
+	}
+	else {
+		diff = inl(PP5002_TIMER1);
+	}
+
+	return USECS_PER_INT - (diff & 0xffff);
 }
 
 extern void ipod_timer_interrupt(int irq, void *dev_id, struct pt_regs *regs)
