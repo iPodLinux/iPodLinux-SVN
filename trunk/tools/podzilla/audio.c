d91688477bc7535606200b0e67513e9036408450
busonerd <busonerd@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2005-02-03 10:52:39 +0000

    Returned insanity to Recording date format.

--- trunk/tools/podzilla/audio.c
+++ trunk/tools/podzilla/audio.c
@@ -572,12 +572,12 @@ void new_record_window()
 	time(&now);
 	tm = localtime(&now);
 
-	/* YYYYMMDD HHMMSS */
-	sprintf(myfilename, "%s/%04d%02d%02d-%02d%02d%02d.wav",
-		RECORDINGS,
-		tm->tm_year + 1900, tm->tm_mon+1, tm->tm_mday, tm->tm_hour,
-		tm->tm_min, tm->tm_sec);
-
+	/* MMDDYYYY HHMMSS */
+	sprintf(myfilename, "%s/%02d%02d%04d %02d%02d%02d.wav",
+			RECORDINGS,
+			tm->tm_mon+1, tm->tm_mday, tm->tm_year + 1900, tm->tm_hour,
+			tm->tm_min, tm->tm_sec);
+	
 	strncpy(pcm_file, myfilename, sizeof(pcm_file) - 1);
 	mode = RECORD;
 
