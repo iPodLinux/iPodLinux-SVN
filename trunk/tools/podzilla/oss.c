d45cf66abfc24876a79f911074204f0f3a95e438
coobert <coobert@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2005-04-19 19:05:32 +0000

    ifdefs for compiling for X11 on non linux systems

--- trunk/tools/podzilla/oss.c
+++ trunk/tools/podzilla/oss.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <sys/types.h>
+#ifdef __linux__
 #include <linux/soundcard.h>
+#endif
 #include <sys/ioctl.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -28,7 +30,11 @@ int dsp_vol_change(dsp_st *oss, int delta)
 		oss->volume = 0;
 	}
 	vol = oss->volume << 8 | oss->volume;
+#ifdef __linux__
 	ret = ioctl(oss->mixer, SOUND_MIXER_WRITE_PCM, &vol);
+#else
+	ret = -1;
+#endif
 	oss->volume = vol & 0xff;
 	return ret;
 }
@@ -50,6 +56,7 @@ int dsp_get_volume(dsp_st *oss)
 
 int dsp_setup(dsp_st *oss, int channels, int rate)
 {
+#ifdef __linux__
 	int ret;
 	int fmt = AFMT_S16_LE;
 	ret = ioctl(oss->dsp, SNDCTL_DSP_SETFMT, &fmt);
@@ -63,15 +70,23 @@ int dsp_setup(dsp_st *oss, int channels, int rate)
 	}
 	/* sample rate */
 	return ioctl(oss->dsp, SNDCTL_DSP_SPEED, &rate);
+#else
+	return -1;
+#endif
 }
 
 static int dsp_set_recsrc(dsp_st *oss, int rec_src)
 {
+#ifdef __linux__
 	return ioctl(oss->mixer, SOUND_MIXER_WRITE_RECSRC, &rec_src);
+#else
+	return -1;
+#endif
 }
 
 void dsp_write(dsp_st *oss, void *ptr, size_t size)
 {
+#ifdef __linux__
 	size_t rem, n;
 	void *p = ptr;
 	if ((rem = size) > 0) {
@@ -83,6 +98,7 @@ void dsp_write(dsp_st *oss, void *ptr, size_t size)
 			}
 		}
 	}
+#endif
 }
 
 size_t dsp_read(dsp_st *oss, void *ptr, size_t size)
@@ -92,6 +108,7 @@ size_t dsp_read(dsp_st *oss, void *ptr, size_t size)
 
 int dsp_open(dsp_st *oss, int mode)
 {
+#ifdef __linux__
 	oss->dsp = open("/dev/dsp", (mode == DSP_LINEOUT)? O_WRONLY : O_RDONLY);
 	if (oss->dsp < 0) {
 		return -1;
@@ -115,6 +132,9 @@ int dsp_open(dsp_st *oss, int mode)
 		}
 	}
 	return 0;
+#else
+	return -1;
+#endif
 }
 
 void dsp_close(dsp_st *oss)
