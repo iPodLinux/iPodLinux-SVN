79bc11c755f4bf7630fcfb1c6134664d2cb17075
courtc <courtc@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2005-07-19 07:29:05 +0000

    colorized; cleaned up a little

--- trunk/tools/podzilla/calc.c
+++ trunk/tools/podzilla/calc.c
@@ -35,22 +35,27 @@ char *num[]={"7","8","9","/","4","5","6","*","1","2","3","-","0",".","=","+"};
 static int cxgap = 5, cygap = 4;
 static int cbw = 25, cbh = 16;
 
-#define CHEIGHT	((5*cbh)+(4*cygap))
-#define CWIDTH	((4*cbw)+(3*cxgap))
-#define CXOFF	((screen_info.cols-CWIDTH)/2)
-#define CYOFF	(((screen_info.rows-(HEADER_TOPLINE+1))-CHEIGHT)/2)
+#define DIV 3
+#define MULT 7
+#define SUB 11
+#define ADD 15
+
+#define CHEIGHT	((cbh*5)+(cygap << 2))
+#define CWIDTH	((cbw << 2)+(cxgap*3))
+#define CXOFF	((screen_info.cols-CWIDTH) >> 1)
+#define CYOFF	(((screen_info.rows-(HEADER_TOPLINE+1))-CHEIGHT) >> 1)
 #define CBYOFF	(CYOFF+cbh+cygap)
 
 static void draw_calc() {
 	GR_SIZE width, height, base;
 
-	GrSetGCUseBackground(calc_gc, GR_FALSE);
-	GrSetGCForeground(calc_gc, BLACK);
+	GrSetGCForeground(calc_gc, appearance_get_color(CS_FG));
 	GrRect(calc_wid, calc_gc, CXOFF, CYOFF, CWIDTH, cbh);
-	GrGetGCTextSize(calc_gc, mathloc, -1, GR_TFASCII, &width, &height, &base);
-	GrSetGCForeground(calc_gc, WHITE);
+	GrGetGCTextSize(calc_gc, mathloc, -1, GR_TFASCII, &width, &height,
+			&base);
+	GrSetGCForeground(calc_gc, appearance_get_color(CS_BG));
 	GrFillRect(calc_wid, calc_gc, CXOFF+1, CYOFF+1, CWIDTH-2, cbh-2);
-	GrSetGCForeground(calc_gc, BLACK);
+	GrSetGCForeground(calc_gc, appearance_get_color(CS_FG));
 	GrText(calc_wid, calc_gc, CXOFF+(CWIDTH-width-4),
 		CYOFF+((cbh/2)-(height/2)), mathloc, -1, GR_TFASCII|GR_TFTOP);
 
@@ -60,41 +65,40 @@ static void draw_calc() {
 		current_calc_item = 0;
 		
 	xlocal=CXOFF+((cbw+cxgap)*(current_calc_item%4));
-	ylocal=CBYOFF+((cbh+cygap)*(current_calc_item/4));
+	ylocal=CBYOFF+((cbh+cygap)*(current_calc_item >> 2));
 	lastxlocal=CXOFF+((cbw+cxgap)*(last_calc_item%4));
-	lastylocal=CBYOFF+((cbh+cygap)*(last_calc_item/4));
+	lastylocal=CBYOFF+((cbh+cygap)*(last_calc_item >> 2));
 	
-	GrSetGCForeground(calc_gc, WHITE);
+	GrSetGCForeground(calc_gc, appearance_get_color(CS_BG));
 	GrFillRect(calc_wid, calc_gc, lastxlocal+1, lastylocal+1, cbw-2, cbh-2);
-	GrSetGCForeground(calc_gc, BLACK);
+	GrSetGCForeground(calc_gc, appearance_get_color(CS_FG));
 	GrGetGCTextSize(calc_gc, num[last_calc_item], -1, GR_TFASCII,
 	                &width, &height, &base);
 	GrText(calc_wid, calc_gc,
 	       lastxlocal+((cbw/2)-(width/2)), lastylocal+((cbh/2)-(height/2)),
 	       num[last_calc_item], -1, GR_TFASCII|GR_TFTOP);
+	GrSetGCForeground(calc_gc, appearance_get_color(CS_SELBG));
 	GrFillRect(calc_wid, calc_gc, xlocal, ylocal, cbw, cbh);
-	GrSetGCForeground(calc_gc, WHITE);
+	GrSetGCForeground(calc_gc, appearance_get_color(CS_SELFG));
 	GrGetGCTextSize(calc_gc, num[current_calc_item], -1, GR_TFASCII,
 	                &width, &height, &base);
 	GrText(calc_wid, calc_gc,
 	       xlocal+((cbw/2)-(width/2)), ylocal+((cbh/2)-(height/2)),
 	       num[current_calc_item], -1, GR_TFASCII|GR_TFTOP);
-	GrSetGCUseBackground(calc_gc, GR_FALSE);
-	GrSetGCMode(calc_gc, GR_MODE_SET);
 }
 
 void calc_do_math(int pos) {
 	static int opr, tog;
-	if((pos==3 || pos==7 || pos==11 || pos>13)&&mathloc[0]!='\0') {
-		//convert string to double, store
+	if((pos==DIV || pos==MULT || pos==SUB || pos>13)&&mathloc[0]!='\0') {
+		/* convert string to double, store */
 		if(numfull) {
-			if(opr==3) //divide
+			if(opr==DIV) /* divide */
 				sprintf(mathloc, "%g", oldnum/atof(mathloc));
-			else if(opr==7) //multiply
+			else if(opr==MULT) /* multiply */
 				sprintf(mathloc, "%g", oldnum*atof(mathloc));
-			else if(opr==11) //subtract
+			else if(opr==SUB) /* subtract */
 				sprintf(mathloc, "%g", oldnum-atof(mathloc));
-			else if(opr==15) //add
+			else if(opr==ADD) /* add */
 				sprintf(mathloc, "%g", oldnum+atof(mathloc));
 		}
 		oldnum = atof(mathloc);
@@ -114,7 +118,7 @@ void calc_do_math(int pos) {
 			tog=0;
 		}
 		littr++;
-		//add number to string
+		/* add number to string */
 		if(littr<16)
 			sprintf(mathloc, "%s%s", mathloc, num[pos]);
 	}
@@ -125,10 +129,7 @@ static void calc_do_draw() {
 	int i;
 	GR_SIZE width, height, base;
 	pz_draw_header("Calculator");
-	mathloc[0]='\0';
-	numfull = 0;
-	littr = 0;
-	GrSetGCForeground(calc_gc, BLACK);
+	GrSetGCForeground(calc_gc, appearance_get_color(CS_FG));
 	for(i=0; i<=15; i++) {
 		GrGetGCTextSize(calc_gc, num[i], -1, GR_TFASCII,
 		                &width, &height, &base);
@@ -148,28 +149,28 @@ static int calc_do_keystroke(GR_EVENT * event) {
 	switch(event->type) {
 	case GR_EVENT_TYPE_KEY_DOWN:
 		switch (event->keystroke.ch) {
-		case '\r':
+		case IPOD_BUTTON_ACTION:
 		case '\n':
 			calc_do_math(current_calc_item);
 			draw_calc();
 			ret |= KEY_CLICK;
 			break;
 
-		case 'l':
+		case IPOD_WHEEL_COUNTERCLOCKWISE:
 			last_calc_item = current_calc_item;
 			current_calc_item--;
 			draw_calc();
 			ret |= KEY_CLICK;
 			break;
 
-		case 'r':
+		case IPOD_WHEEL_CLOCKWISE:
 			last_calc_item = current_calc_item;
 			current_calc_item++;
 			draw_calc();
 			ret |= KEY_CLICK;
 			break;
 
-		case 'm':
+		case IPOD_BUTTON_MENU:
 			GrDestroyGC(calc_gc);
 			pz_close_window(calc_wid);
 			ret |= KEY_CLICK;
@@ -190,14 +191,21 @@ static int calc_do_keystroke(GR_EVENT * event) {
 void new_calc_window() {
 	calc_gc = pz_get_gc(1);
 	GrSetGCUseBackground(calc_gc, GR_FALSE);
-	GrSetGCForeground(calc_gc, BLACK);
+	GrSetGCForeground(calc_gc, appearance_get_color(CS_FG));
 
-	if (screen_info.cols < 160) { //mini
+	mathloc[0]='\0';
+	numfull = 0;
+	littr = 0;
+
+	if (screen_info.cols < 160) { /* mini */
 		cxgap = 3; cygap = 2;
 		cbw = 22; cbh = 15;
 	}
 
-	calc_wid = pz_new_window(0, HEADER_TOPLINE + 1, screen_info.cols, screen_info.rows - (HEADER_TOPLINE + 1), calc_do_draw, calc_do_keystroke);
+	calc_wid = pz_new_window(0, HEADER_TOPLINE + 1, screen_info.cols,
+			screen_info.rows - (HEADER_TOPLINE + 1), calc_do_draw,
+			calc_do_keystroke);
+	GrSetWindowBackgroundColor(calc_wid, appearance_get_color(CS_BG));
 
 	GrSelectEvents(calc_wid, GR_EVENT_MASK_EXPOSURE |
 			GR_EVENT_MASK_KEY_UP | GR_EVENT_MASK_KEY_DOWN);
