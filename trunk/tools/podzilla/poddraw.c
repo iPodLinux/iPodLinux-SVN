3299704d12d8d9fee00d1527fb326a624c5143be
yorgle <yorgle@gmail.com>
2005-07-21 02:49:02 +0000

    Added color. clear screen now clears to the current color. The current draw pixel color is always displayed in the top left. Fixed indenting to be consistant with other modules.

--- trunk/tools/podzilla/poddraw.c
+++ trunk/tools/podzilla/poddraw.c
@@ -48,121 +48,155 @@ static int updown = 0;
 #define PDMIN( A, B )   (((A)<(B))?(A):(B))
 #define PDMAX( A, B )   (((A)>(B))?(A):(B))
 
+static GR_COLOR colors[] = {
+	BLACK,
+	GRAY,
+	LTGRAY,
+	WHITE,
+
+        GR_RGB( 255,   0,   0 ),
+        GR_RGB( 255, 128,   0 ),
+        GR_RGB( 255, 255,   0 ),
+	GR_RGB( 128, 255,   0 ),
+
+        GR_RGB(   0, 255,   0 ),
+        GR_RGB(   0, 255, 128 ),
+        GR_RGB(   0, 255, 255 ),
+        GR_RGB(   0, 128, 255 ),
+
+        GR_RGB(   0,   0, 255 ),
+        GR_RGB( 128,   0, 255 ),
+        GR_RGB( 255,   0, 255 ),
+        GR_RGB( 255,   0, 128 ),
+
+};
+
+static void poddraw_draw_box( GR_COLOR color, int x, int y )
+{
+	GrSetGCForeground( poddraw_gc, color );
+
+	GrPoint( poddraw_wid, poddraw_gc, x,   y );
+	GrPoint( poddraw_wid, poddraw_gc, x+1, y+1 );
+	GrPoint( poddraw_wid, poddraw_gc, x+1, y );
+	GrPoint( poddraw_wid, poddraw_gc, x,   y+1 );
+}
+
+
 static void poddraw_place_point( int c )
 {
-    if( (c & 0x03) == 0 ) 
-	GrSetGCForeground( poddraw_gc, BLACK);
-    else if( (c & 0x03) == 1 ) 
-	GrSetGCForeground( poddraw_gc, GRAY);
-    else if( (c & 0x03) == 2 ) 
-	GrSetGCForeground( poddraw_gc, LTGRAY);
-    else if( (c & 0x03) == 3 ) 
-	GrSetGCForeground( poddraw_gc, WHITE);
-
-    GrPoint( poddraw_wid, poddraw_gc, poddraw_x,   poddraw_y );
-    GrPoint( poddraw_wid, poddraw_gc, poddraw_x+1, poddraw_y+1 );
-    GrPoint( poddraw_wid, poddraw_gc, poddraw_x+1, poddraw_y );
-    GrPoint( poddraw_wid, poddraw_gc, poddraw_x,   poddraw_y+1 );
+	poddraw_draw_box( colors[ c&((screen_info.bpp==16)?0x0f:0x03) ],
+			poddraw_x, poddraw_y );
 }
 
-static void poddraw_point( void )
-{
-    poddraw_place_point( poddraw_color );
-}
 
 static void poddraw_cycle_point( void )
 {
-    static int c = 0;
-    poddraw_place_point( c++ );
+	static int c = 0;
+	poddraw_place_point( c++ );
 }
+
+
+static void poddraw_cls( void )
+{
+	GrSetGCForeground( poddraw_gc, colors[poddraw_color]);
+
+	GrFillRect( poddraw_wid, poddraw_gc, 0, 0, 
+		    screen_info.cols,
+		    screen_info.rows - HEADER_TOPLINE );
+}
+
 
 static int poddraw_handle_event(GR_EVENT * event)
 {
-    int ret = 0;
-    switch( event->type )
-    {
-    case( GR_EVENT_TYPE_TIMER ):
-	poddraw_cycle_point();
-	break;
-	
-
-    case( GR_EVENT_TYPE_KEY_DOWN ):
-	switch( event->keystroke.ch )
+	int ret = 0;
+	switch( event->type )
 	{
-	    case IPOD_BUTTON_ACTION: /* action */
-		// toggle lr-ud
-		updown = (updown + 1) & 0x01;
+	case( GR_EVENT_TYPE_TIMER ):
+		poddraw_cycle_point();
 		break;
+	    
+	case( GR_EVENT_TYPE_KEY_DOWN ):
+		switch( event->keystroke.ch )
+		{
+		    case IPOD_BUTTON_ACTION: /* action */
+			    // toggle lr-ud
+			    updown = (updown + 1) & 0x01;
+			    break;
 
-	    case IPOD_BUTTON_PLAY: /* play/pause */
-		// clear screen
-		GrSetGCBackground( poddraw_gc, WHITE);
-		GrClearWindow( poddraw_wid, 0);
-		break;
+		    case IPOD_BUTTON_PLAY: /* play/pause */
+			    // clear screen
+			    poddraw_cls();
+			    break;
 
-	    case IPOD_BUTTON_FORWARD: /* >>| */
-		poddraw_color = (poddraw_color +1 ) & 0x03;
-		break;
+		    case IPOD_BUTTON_FORWARD: /* >>| */
+			    poddraw_color = (poddraw_color +1 ) & 
+				(( screen_info.bpp == 16 )?0x0f:0x03);
+			    poddraw_draw_box( colors[poddraw_color], 0, 0 );
+			    break;
 
-	    case IPOD_BUTTON_REWIND: /* |<< */
-		poddraw_color = (poddraw_color -1 ) & 0x03;
-		break;
+		    case IPOD_BUTTON_REWIND: /* |<< */
+			    poddraw_color = (poddraw_color -1 ) & 
+				(( screen_info.bpp == 16 )?0x0f:0x03);
+			    poddraw_draw_box( colors[poddraw_color], 0, 0 );
+			    break;
 
-	    case IPOD_WHEEL_ANTICLOCKWISE: /* CCW spin */
-		poddraw_point();
-		if( updown ) poddraw_x = PDMAX( poddraw_x-2, 0 );
-		else         poddraw_y = PDMAX( poddraw_y-2, 0 );
-		break;
+		    case IPOD_WHEEL_ANTICLOCKWISE: /* CCW spin */
+			    poddraw_place_point( poddraw_color );
+			    if( updown ) poddraw_x = PDMAX( poddraw_x-2, 0 );
+			    else         poddraw_y = PDMAX( poddraw_y-2, 0 );
+			    break;
 
-	    case IPOD_WHEEL_CLOCKWISE: /* CW spin */
-		poddraw_point();
-		if( updown )
-		    poddraw_x = PDMIN( poddraw_x+2, screen_info.cols-2 );
-		else
-		    poddraw_y = PDMIN( poddraw_y+2, 
-				  (screen_info.rows - (HEADER_TOPLINE + 1))-2);
-		break;
+		    case IPOD_WHEEL_CLOCKWISE: /* CW spin */
+			    poddraw_place_point( poddraw_color );
+			    if( updown )
+				poddraw_x = PDMIN( poddraw_x+2, 
+						    screen_info.cols-2 );
+			    else
+				poddraw_y = PDMIN( poddraw_y+2, 
+					  (screen_info.rows - 
+						(HEADER_TOPLINE + 1))-2);
+			break;
 
-	    case IPOD_BUTTON_MENU:
-		GrDestroyTimer( poddraw_timer );
-		pz_close_window( poddraw_wid );
-		ret = 1;
-		break;
+		    case IPOD_BUTTON_MENU:
+		    case ('q'):
+			    GrDestroyTimer( poddraw_timer );
+			    pz_close_window( poddraw_wid );
+			    ret = 1;
+			    break;
 
-	    default:
-		ret |= EVENT_UNUSED;
+		    default:
+			    ret |= EVENT_UNUSED;
+			    break;
+		}
 		break;
 	}
-	break;
-    }
-
-    return ret;
+	return ret;
 }
 
 static void poddraw_do_draw( void )
 {
-    pz_draw_header( "PodDraw" );
-    poddraw_cycle_point();
+	pz_draw_header( "PodDraw" );
+	poddraw_cycle_point();
 }
 
 void new_poddraw_window( void )
 {
-    poddraw_gc = pz_get_gc(1);
-    GrSetGCUseBackground(poddraw_gc, GR_FALSE);
-    GrSetGCForeground(poddraw_gc, BLACK);
+	poddraw_gc = pz_get_gc(1);
+	GrSetGCUseBackground(poddraw_gc, GR_FALSE);
+	GrSetGCForeground(poddraw_gc, BLACK);
 
-    poddraw_wid = pz_new_window(0, HEADER_TOPLINE + 1, 
-	screen_info.cols, screen_info.rows - (HEADER_TOPLINE + 1), 
-	poddraw_do_draw, poddraw_handle_event);
+	poddraw_wid = pz_new_window(0, HEADER_TOPLINE + 1, 
+	    screen_info.cols, screen_info.rows - (HEADER_TOPLINE + 1), 
+	    poddraw_do_draw, poddraw_handle_event);
 
-    GrSelectEvents( poddraw_wid, GR_EVENT_MASK_TIMER|
-	GR_EVENT_MASK_EXPOSURE|GR_EVENT_MASK_KEY_UP|GR_EVENT_MASK_KEY_DOWN);
+	GrSelectEvents( poddraw_wid, GR_EVENT_MASK_TIMER|
+	    GR_EVENT_MASK_EXPOSURE|GR_EVENT_MASK_KEY_UP|GR_EVENT_MASK_KEY_DOWN);
 
-    poddraw_timer = GrCreateTimer( poddraw_wid, 100 );
+	poddraw_timer = GrCreateTimer( poddraw_wid, 100 );
 
-    GrMapWindow( poddraw_wid );
-    poddraw_color = 0;
-    poddraw_x = screen_info.cols/2;
-    poddraw_y = (screen_info.rows - (HEADER_TOPLINE + 1))/2;
-
+	GrMapWindow( poddraw_wid );
+	poddraw_color = 0;
+	poddraw_x = screen_info.cols/2;
+	poddraw_y = (screen_info.rows - (HEADER_TOPLINE + 1))/2;
+	poddraw_cls();
 }
