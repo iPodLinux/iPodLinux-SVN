a425c0d1c2cb634f2b5cabcedfe5662f554ba42e
josh <josh@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2005-10-18 17:46:51 +0000

    Not a full SMTP server anymore.

--- management/svnbot.pl
+++ management/svnbot.pl
@@ -3,84 +3,11 @@
 use IO::Socket;
 use Net::IRC;
 use Data::Dumper;
+
 use POSIX qw/getpid setsid/;
 
-$PORT = 25;
-
-$irc = new Net::IRC;
-$conn = $irc->newconn( Server   => 'irc.freenode.net',
-		       Port     => 6667,
-		       Nick     => 'iPL-SVN',
-		       Ircname  => 'iPodLinux SVN-tracking bot',
-		       Username => 'iplsvn');
-
-$SIG{'CHLD'} = sub { wait; };
-
-sub do_smtp_stuff {
-    $server = IO::Socket::INET->new( Proto     => 'tcp',
-				     LocalPort => $PORT,
-				     Listen    => SOMAXCONN,
-				     Address   => '0.0.0.0',
-				     Reuse     => 1);
-
-    die "can't setup server" unless $server;
-    print "[Server $0 accepting clients on port $PORT]\n";
-
-    my($client);
-    while ($client = $server->accept()) {
-	my $pid = fork();
-	die "Cannot fork" unless defined $pid;
-	do { close $client; next; } if $pid; # parent
-	
-	my($address) = "";
-	my($data) = "";
-	my($datamode) = 0;
-	
-	$client->autoflush(1);
-	print $client "220 this is svnbot.pl at your service\n";
-	while (<$client>) {
-	    s/\n//g; s/\r//g;
-
-	    if ($datamode) {
-		if (/^.$/) {
-		    $datamode = 0;
-		    print $client "250 ok, queued\n";
-		    parse_data($data);
-		} else {
-		    $data .= $_ ."\n";
-		}
-	    } else {
-		next unless /\S/;       # blank line
-		if (/^quit/i) {
-		    print $client "221 bye\n";
-		    close $client;
-		    exit 0;
-		} elsif (/^helo/i or /^ehlo/i) {
-		    print $client "250 hello to you too\n";
-		} elsif (/^mail/i) {
-		    print $client "250 ok, ignored\n";
-		} elsif (/^rcpt to:\s*<?([-A-Za-z_.+0-9]*)@([-A-Za-z_.0-9]*)>?/i) {
-		    $address = $1;
-		    $domain = $2;
-		    if ($domain eq "get-linux.org") {
-			print $client "250 ok, sending to $1\n";
-		    } else {
-			print $client "551 buzz off, I'm not an open relay\n";
-			print $client "221 goodbye, spammer\n";
-			close $client;
-			exit 0;
-		    }
-		} elsif (/^data/i) {
-		    $datamode = 1;
-		    print $client "354 go ahead\n";
-		} else {
-		    print $client "502 go read rfc821\n";
-		}
-	    }
-	}
-    }
-}
-
+my($CHANNEL) = "#ipodlinux.bot";
+my($KEY)     = "";
 
 sub parse_data($) {
     my(@data) = split /\n/, $_[0];
@@ -153,7 +80,7 @@ sub parse_data($) {
 	}
     }
 
-    $conn->privmsg ("#ipodlinux.dev", "03$user * $flist: $log ($tracurl)");
+    $conn->privmsg ($CHANNEL, "03$user * $flist: $log ($tracurl)");
 }
 
 sub on_msg {
@@ -187,20 +114,29 @@ sub on_msg {
 
 sub on_connect {
     my $self = shift;
-    $self->join("#ipodlinux.dev", "rs232");
+    $self->join($CHANNEL, $KEY);
     $self->privmsg("nickserv", "identify ipsofatso");
-
-    do_smtp_stuff unless fork;
 }
 
 my($bg) = 1;
 
 if (defined($ARGV[0])) {
     if ($ARGV[0] eq "-k") {
+	open PIDFILE, ".svnc/svnbot.pid" or die "svnbot not running\n";
+	$pid = <PIDFILE>; chomp $pid; close PIDFILE;
+	unlink ".svnc/svnbot.pid";
+	kill -15, $pid or exit 1;
+	exit 0;
+    } elsif ($ARGV[0] eq "-q") {
+	# queue message
+	-d ".svnc" or mkdir ".svnc";
+	chdir ".svnc";
+	open FILE, ">".time.".".getpid.".svn";
+	print FILE while <STDIN>;
+	close FILE;
 	open PIDFILE, "svnbot.pid" or die "svnbot not running\n";
 	$pid = <PIDFILE>; chomp $pid; close PIDFILE;
-	unlink "svnbot.pid";
-	kill -15, $pid or exit 1;
+	kill 1, $pid or die "unable to signal svnbot\n";
 	exit 0;
     } elsif ($ARGV[0] eq "-f") {
 	$bg = 0;
@@ -210,11 +146,39 @@ if (defined($ARGV[0])) {
 if ($bg) {
     exit 0 if fork; setsid;
 }
-	
-open PIDFILE, ">svnbot.pid" and do {
-    print PIDFILE getpid;
-    close PIDFILE;
+
+$SIG{'HUP'} = sub {
+    chdir ".svnc";
+    opendir SVNC, "." or do {
+	$conn->privmsg($CHANNEL, "I'm misconfigured. Bye.");
+	exit 1;
+    };
+    @newfiles = grep { /^[0-9]*\.[0-9]*\.svn$/ } readdir SVNC;
+    closedir SVNC;
+    for (@newfiles) {
+	local $/ = 0;
+	open THISF, $_;
+	my($data) = <THISF>;
+	close THISF;
+
+	print "== $_ ==\n";
+        print <THISF>;
+	print "<< End >>\n\n";
+
+	parse_data $data;
+    }
 };
+
+open PIDFILE, ">.svnc/svnbot.pid" or die "Can't open PID file; do you need to mkdir .svnc?\n";
+print PIDFILE getpid;
+close PIDFILE;
+
+$irc = new Net::IRC;
+$conn = $irc->newconn( Server   => 'irc.freenode.net',
+		       Port     => 6667,
+		       Nick     => 'iPL-SVN',
+		       Ircname  => 'iPodLinux SVN-tracking bot',
+		       Username => 'iplsvn');
 
 $conn->add_global_handler(376, \&on_connect);
 $conn->add_handler("msg", \&on_msg);
