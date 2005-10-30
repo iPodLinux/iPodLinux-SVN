6ee22b5c21d2d889beba6202d6389ad30685ea62
josh <josh@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2005-10-30 22:34:41 +0000

    Use our own lndir.

--- tools/ttk/make-build-dirs.sh
+++ tools/ttk/make-build-dirs.sh
@@ -4,13 +4,17 @@ PATH=$PATH:/usr/X11R6/bin
 
 builddirs="ipod-sdl x11-sdl ipod-mwin x11-mwin"
 
+cc -o lndir lndir.c
+
 mkdir -p build
 cd build
 
 for dir in $builddirs; do
 	mkdir -p $dir
 	cd $dir
-	lndir ../../src
+	../../lndir ../../src
 	cd ..
 done
-	
+
+cd ..
+rm lndir
