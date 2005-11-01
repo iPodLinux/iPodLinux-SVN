821be6904368ba58637e1c47d817e4e19c9641e0
slowcoder <slowcoder@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2005-11-01 23:26:07 +0000

    Clipping for PNGs and fonts implemented

--- tools/hotdog/hotdog_png.c
+++ tools/hotdog/hotdog_png.c
@@ -42,28 +42,54 @@ void HD_PNG_Render(hd_engine *eng,hd_object *obj) {
   
   fp_step_x = (obj->sub.png->w << 16) / obj->w;
   fp_step_y = (obj->sub.png->h << 16) / obj->h;
-  
-#if 1
-  if( (obj->x >= 0) && ((obj->x+obj->w) < eng->screen.width) &&
-      (obj->y >= 0) && ((obj->y+obj->h) < eng->screen.height) ) {
-    
-    startx = 0;
-    starty = 0;
-    endx   = obj->w;
-    endy   = obj->h;
+
+  // 1st, check if we need to do anything at all
+  if( (obj->x > eng->screen.width) || (obj->y > eng->screen.height) ||
+	  ((obj->x+obj->w) < 0) || ((obj->y+obj->h) < 0) ) {
+
+	// Nope, we're entirely off-screen, so there's nothing for us to do
+	return;
+
+  // 2nd, check if we're completely inside the screen
+  } else if( (obj->x >= 0) && ((obj->x+obj->w) < eng->screen.width) &&
+             (obj->y >= 0) && ((obj->y+obj->h) < eng->screen.height) ) {
+	
+    startx = obj->x;
+    starty = obj->y;
+    endx   = obj->x+obj->w;
+    endy   = obj->y+obj->h;
 
     fp_initial_ix = 0;
     fp_initial_iy = 0;
   } else {
+    starty = obj->y;
+	startx = obj->x;
+    endy   = obj->y+obj->h;
+    endx   = obj->x+obj->w;
+    fp_initial_ix = 0;
+    fp_initial_iy = 0;
 
     // Let the clipping commence
-
-    if( obj->x < 0 ) { startx = 0; fp_initial_ix = -(obj->x) * fp_step_x; }
-
+    if( obj->x < 0 ) {
+		startx = 0;
+		endx   = obj->w + obj->x;
+		fp_initial_ix = -(obj->x) * fp_step_x;
+	}
+	if( (obj->x+obj->w) > eng->screen.width ) {
+		endx = eng->screen.width - 1;
+	}
+    if( obj->y < 0 ) {
+		starty = 0;
+		endy   = obj->h + obj->y;
+		fp_initial_iy = -(obj->y) * fp_step_y;
+	}
+	if( (obj->y+obj->h) > eng->screen.height ) {
+		endy = eng->screen.height - 1;
+	}
+	
   }
-#endif
   
-  buffOff = obj->y * eng->screen.width + obj->x;
+  buffOff = obj->y * eng->screen.width;// + startx;
   imgOff  = 0;
   
   fp_iy = fp_initial_iy;
