abf3545d1509a112d412dca244bc0bda185caf3a
slowcoder <slowcoder@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2005-11-26 18:22:26 +0000

    Note to self: Make sure it compiles before checking in

--- tools/ipodloader2/keypad.c
+++ tools/ipodloader2/keypad.c
@@ -1,9 +1,7 @@
 #include "bootloader.h"
 #include "ipodhw.h"
 
-static ipod_t ipod;
-
-
+static ipod_t *ipod;
 
 int keypad_getstate_opto(void) {
   uint32 in,st,button;
@@ -34,11 +32,11 @@ begin:
 int keypad_getstate(void) {
   uint8 state;
 
-  if( (ipod.hw_rev>>16) >= 5 ) return( keypad_getstate_opto() );
+  if( (ipod->hw_rev>>16) >= 5 ) return( keypad_getstate_opto() );
   
   // REW, Menu, PLay, FF
 
-  if( (ipod.hw_rev>>16) < 4 ) {
+  if( (ipod->hw_rev>>16) < 4 ) {
     state = inb(0xCF000030);
     if(     state & 0x10 ) return(IPOD_KEYPAD_UP);
     else if(state & 0x04 ) return(IPOD_KEYPAD_DOWN);
@@ -47,12 +45,13 @@ int keypad_getstate(void) {
     if(     state & 0x02 ) return(IPOD_KEYPAD_UP);
     else if(state & 0x04 ) return(IPOD_KEYPAD_DOWN);
   }
+  return(0);
 }
 
 void keypad_init(void) {
   ipod = ipod_get_hwinfo();
 
-  if( (ipod.hw_rev>>16) < 4 ) {
+  if( (ipod->hw_rev>>16) < 4 ) {
     /* 1G -> 3G Keyboard init */
     outb(~inb(0xcf000030), 0xcf000060);
     outb(inb(0xcf000040), 0xcf000070);
@@ -62,11 +61,11 @@ void keypad_init(void) {
     outb(inb(0xcf000024) | 0x1, 0xcf000024);
     
     outb(0xff, 0xcf000050);
-  } else if( (ipod.hw_rev>>16) == 4 ) {
+  } else if( (ipod->hw_rev>>16) == 4 ) {
     /* mini keyboard init */
     outl(inl(0x6000d000) | 0x3f, 0x6000d000);
     outl(inl(0x6000d010) & ~0x3f, 0x6000d010);
-  } else if( (ipod.hw_rev>>16) >= 5 ) {
+  } else if( (ipod->hw_rev>>16) >= 5 ) {
     // IAK
     outl(0,0x7000C180);
     
