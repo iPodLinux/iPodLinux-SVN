2afa17b98e2a14ce70ae1d425ab6b1b9c8a37686
josh <josh@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2005-11-27 00:26:14 +0000

    Add GPL and GPL notices.

--- tools/ttk/src/include/icons.h
+++ tools/ttk/src/include/icons.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2005 Joshua Oreman
+ *
+ * This file is a part of TTK.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 #define TTK_ICON_WIDTH   8
 #define TTK_ICON_HEIGHT  13
 extern unsigned char ttk_icon_sub[], ttk_icon_spkr[], ttk_icon_play[], ttk_icon_pause[], ttk_icon_exe[],
