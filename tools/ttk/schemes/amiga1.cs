d67edefae468626696401b6c0bd52c0b33224386
bleullama <bleullama@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2005-11-30 20:47:07 +0000

    Added amiga color schemes back into it all

---
+++ tools/ttk/schemes/amiga1.cs
@@ -0,0 +1,41 @@
+\name Amiga 1.x
+
+\def blue   #0055AA
+\def black  #000022
+\def white  #ffffff
+\def orange #ff8800
+
+
+  header: bg => white, fg => blue, line => blue -1, accent => blue
+
+ battery: border => black, bg => white, fill.normal => blue +1, 
+		fill.low => orange +1, fill.charge => orange +1
+
+    lock: border => orange, fill => black
+
+ loadavg: bg => blue, fg => white, spike => orange
+
+  window: bg => blue, fg => white, border => orange -3
+
+  dialog: bg => blue, fg => white, line => white,
+          title.fg => white,
+          button.bg => blue, button.fg => white, button.border => white,
+	  button.sel.bg => white, button.sel.fg => black, button.sel.border => white, button.sel.inner => black +1
+
+  error:  bg => blue, fg => white, line => orange,
+          title.fg => white,
+          button.bg => blue, button.fg => white, button.border => white,
+	  button.sel.bg => white, button.sel.fg => black, button.sel.border => white, button.sel.inner => black +1
+
+  scroll: box => white, bg => black +1, bar => orange +2
+
+   input: bg => white, fg => black, selbg => orange, selfg => black,
+	  border => black, cursor => orange
+
+    menu: bg => blue, fg => white, choice => white, icon => orange,
+          selbg => orange, selfg => black,
+	  selchoice => white, selicon => black
+
+  slider: border => white, bg => black, full => orange
+
+textarea: bg => blue, fg => white
---
+++ tools/ttk/schemes/amiga2.cs
@@ -0,0 +1,41 @@
+\name Amiga 2.x
+
+\def gray   #aaaaaa
+\def white  #ffffff
+\def black  #000000
+\def blue   #83acd6
+
+
+  header: bg => blue, fg => black, line => white -1, accent => black
+
+ battery: border => black, bg => black, fill.normal => black +1, 
+		fill.low => white +1, fill.charge => black +1
+
+    lock: border => black, fill => black
+
+ loadavg: bg => gray, fg => white, spike => blue
+
+  window: bg => gray, fg => black, border => white -3
+
+  dialog: bg => blue, fg => black, line => gray,
+          title.fg => black,
+          button.bg => blue, button.fg => black, button.border => white,
+	  button.sel.bg => black, button.sel.fg => white, button.sel.border => wblack, button.sel.inner => white +1
+
+  error:  bg => blue, fg => black, line => white,
+          title.fg => black,
+          button.bg => blue, button.fg => black, button.border => white,
+	  button.sel.bg => black, button.sel.fg => white, button.sel.border => wblack, button.sel.inner => white +1
+
+  scroll: box => blue, bg => black +1, bar => white +2
+
+   input: bg => gray, fg => black, selbg => blue, selfg => black,
+	  border => black, cursor => white
+
+    menu: bg => gray, fg => black, choice => white, icon => black,
+          selbg => black, selfg => white,
+	  selchoice => blue, selicon => black
+
+  slider: border => white, bg => black, full => blue
+
+textarea: bg => gray, fg => black
