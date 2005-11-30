b8e23a8d7e0cc969bc7edf0182781d6c47a415d1
oremanj <oremanj@rwcr.net>
2005-11-30 19:54:22 +0000

    Icons fix.

--- tools/ttk/src/include/icons.h
+++ tools/ttk/src/include/icons.h
@@ -23,4 +23,4 @@
 extern unsigned char ttk_icon_sub[], ttk_icon_spkr[], ttk_icon_play[], ttk_icon_pause[], ttk_icon_exe[],
     ttk_icon_battery[], ttk_icon_charging[], ttk_icon_hold[];
 
-void ttk_draw_icon (unsigned char *icon, ttk_surface srf, int x, int y, ttk_color col, int inv);
+void ttk_draw_icon (unsigned char *icon, ttk_surface srf, int x, int y, ttk_color fgcol, ttk_color bgcol);
