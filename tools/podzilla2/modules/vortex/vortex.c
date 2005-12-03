aa0ecc4ac29b32d1af3a1ef3cd97f4c4a3067b78
yorgle <yorgle@gmail.com>
2005-12-03 21:05:06 +0000

    Made the indenting consistant with the rest of podzilla because people apparantly can't deal with the difference between 4 spaces and 8.  (myself included, it would seem)

--- tools/podzilla2/modules/vortex/vortex.c
+++ tools/podzilla2/modules/vortex/vortex.c
@@ -39,10 +39,11 @@ void draw_vortex (PzWidget *widget, ttk_surface srf)
 
 	Vortex_Console_Render( srf, ttk_makecol( BLACK ));
 
-	if( Vortex_Console_GetZoomCount() == 0 )
+	if( Vortex_Console_GetZoomCount() == 0 ) {
 		pz_vector_string_center( srf, "[menu] to exit.",
                             (ttk_screen->w - ttk_screen->wx)>>1, 10,
                             5, 9, 1, ttk_makecol( BLACK ));
+	}
 
 /*
 	//ttk_line (srf, widget->x + 5, 10, widget->x + widget->w - 5, 10, ttk_makecol (DKGREY));
@@ -58,9 +59,8 @@ void cleanup_vortex()
 
 int event_vortex (PzEvent *ev) 
 {
-    switch (ev->type) {
-    case PZ_EVENT_SCROLL:
-	{
+	switch (ev->type) {
+	case PZ_EVENT_SCROLL:
 		TTK_SCROLLMOD( ev->arg, 4 );
 
 		if( ev->arg > 0 ) {
@@ -72,58 +72,57 @@ int event_vortex (PzEvent *ev)
 					Vortex_Rand(4)-2, Vortex_Rand(4)-2, 
 					VORTEX_STYLE_NORMAL );
 		}
-	}
-	break;
-
-    case PZ_EVENT_BUTTON_UP:
-	switch( ev->arg ) {
-	    case( PZ_BUTTON_MENU ):
-		pz_close_window (ev->wid->win);
 		break;
 
-	    default:
+	case PZ_EVENT_BUTTON_UP:
+		switch( ev->arg ) {
+		case( PZ_BUTTON_MENU ):
+			pz_close_window (ev->wid->win);
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case PZ_EVENT_DESTROY:
+		cleanup_vortex();
+		break;
+
+	case PZ_EVENT_TIMER:
+		Vortex_Console_Tick();
+		ev->wid->dirty = 1;
 		break;
 	}
-	break;
-
-    case PZ_EVENT_DESTROY:
-	cleanup_vortex();
-	break;
-
-    case PZ_EVENT_TIMER:
-	Vortex_Console_Tick();
-	ev->wid->dirty = 1;
-	break;
-    }
-    return 0;
+	return 0;
 }
 
 PzWindow *new_vortex_window()
 {
-    PzWindow *window;
-    FILE *fp;
-    
-    //fp = fopen (pz_module_get_datapath (module, "message.txt"), "r");
+	PzWindow *window;
+	FILE *fp;
+	
+	//fp = fopen (pz_module_get_datapath (module, "message.txt"), "r");
 
-    srand( time( NULL ));
-    Vortex_Console_Init();
+	srand( time( NULL ));
+	Vortex_Console_Init();
 
-    window = pz_new_window( "Vortex", PZ_WINDOW_NORMAL );
-    widget = pz_add_widget( window, draw_vortex, event_vortex );
-    pz_widget_set_timer( widget, 40 );
+	window = pz_new_window( "Vortex", PZ_WINDOW_NORMAL );
+	widget = pz_add_widget( window, draw_vortex, event_vortex );
+	pz_widget_set_timer( widget, 40 );
 
-    Vortex_Console_AddItem( "VORTEX", 0, 0, VORTEX_STYLE_NORMAL );
+	Vortex_Console_AddItem( "VORTEX", 0, 0, VORTEX_STYLE_NORMAL );
 
-    return pz_finish_window( window );
+	return pz_finish_window( window );
 }
 
 void init_vortex() 
 {
-    /* internal module name */
-    module = pz_register_module ("vortex", cleanup_vortex);
+	/* internal module name */
+	module = pz_register_module ("vortex", cleanup_vortex);
 
-    /* menu item display name */
-    pz_menu_add_action ("/Extras/Games/Vortex WIP", new_vortex_window);
+	/* menu item display name */
+	pz_menu_add_action ("/Extras/Games/Vortex WIP", new_vortex_window);
 }
 
 PZ_MOD_INIT (init_vortex)
