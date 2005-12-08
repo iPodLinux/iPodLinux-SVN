2bf10f94bfd0614b7edf950ab45f2737e24f4091
coob <coob@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2005-12-08 04:15:58 +0000

    + GPL

--- tools/hotdog/hdpong/hdpong.c
+++ tools/hotdog/hdpong/hdpong.c
@@ -1,7 +1,19 @@
 /*
- * Copyright (c) 2005, Alastair Stuart
- * All rights reserved.
+ * Copyright (C) 2004, 2005 Courtney Cavin, Alastair Stuart
  *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
  
 #include <stdio.h>
@@ -244,7 +256,7 @@ int main(int argc, char *argv[]) {
 			state = RBEGIN;
 			printf("COM: %d | PLAYER: %d\n", comppoint, userpoint);
 			
-			if (comppoint == 2 || userpoint == 2) state = GOVER;
+			if (comppoint == 10 || userpoint == 10) state = GOVER;
 		}
 
 		break;
