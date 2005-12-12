a4198a28f5861c7a6e8622e854c024de6aeaac48
slowcoder <slowcoder@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2005-12-12 18:03:34 +0000

    Now with configurable font-sizes

--- tools/ipodloader2/console.c
+++ tools/ipodloader2/console.c
@@ -4,7 +4,7 @@
 #include "minilibc.h"
 #include "ipodhw.h"
 
-#include "vga_font.h"
+/*#include "vga_font.h"*/
 
 static struct {
   struct {
@@ -30,9 +30,9 @@ void console_setcolor(uint16 fg,uint16 bg,uint8 transparent) {
 void console_blitchar(int x,int y,char ch) {
   int r,c;
 
-  for(r=0;r<16;r++) {
-    for(c=0;c<8;c++) {
-      if( (uint8)font8x16[(uint8)ch][r] & (1<<(8-c)) ) {  /* Pixel set */
+  for(r=0;r<FONT_HEIGHT;r++) {
+    for(c=0;c<FONT_WIDTH;c++) {
+      if( (uint8)fontdata[(uint8)ch * FONT_HEIGHT + r] & (1<<(8-c)) ) {  /* Pixel set */
 	console.fb[(y+r)*console.dimensions.w+x+c] = console.fgcolor;
       } else { /* Pixel clear */
 	if( !console.transparent )
@@ -45,8 +45,8 @@ void console_blitchar(int x,int y,char ch) {
 void console_putchar(char ch) {
   int32 x,y;
 
-  x = console.cursor.x * VGA_FONT_WIDTH;
-  y = console.cursor.y * VGA_FONT_HEIGHT;
+  x = console.cursor.x * FONT_WIDTH;
+  y = console.cursor.y * FONT_HEIGHT;
 
   if(ch == '\n') { 
     console.cursor.x = 0;
@@ -54,16 +54,16 @@ void console_putchar(char ch) {
 
 #if THIS_CORRUPTS_MEMORY_SOMEHOW
     /* Check if we need to scroll the display up */
-    if(console.cursor.y > (console.dimensions.h/VGA_FONT_HEIGHT) ) {
+    if(console.cursor.y > (console.dimensions.h/FONT_HEIGHT) ) {
       mlc_memcpy(console.fb,
-		 console.fb+(console.dimensions.w*VGA_FONT_HEIGHT*2),
+		 console.fb+(console.dimensions.w*FONT_HEIGHT*2),
 		 (console.dimensions.w*console.dimensions.h*2) - 
-		 (console.dimensions.w*VGA_FONT_HEIGHT*2) );
+		 (console.dimensions.w*FONT_HEIGHT*2) );
     }
     fb_update(console.fb);
     return;
 #else
-    if( (y+VGA_FONT_HEIGHT) >= console.dimensions.h ) {
+    if( (y+FONT_HEIGHT) >= console.dimensions.h ) {
       console.cursor.x = 0;
       console.cursor.y = 0;
       x = 0;
@@ -78,57 +78,11 @@ void console_putchar(char ch) {
 
   console.cursor.x += 1;
 
-  if( console.cursor.x > (console.dimensions.w/VGA_FONT_WIDTH) ) {
+  if( console.cursor.x > (console.dimensions.w/FONT_WIDTH) ) {
     console.cursor.x = 0; 
     console.cursor.y++;
   }
 
-}
-
-void console_putcharX(char ch) {
-  int32 r,c,x,y;
-
-  x = console.cursor.x * VGA_FONT_WIDTH;
-  y = console.cursor.y * VGA_FONT_HEIGHT;
-
-  if(ch == '\n') { 
-    console.cursor.x = 0;
-    console.cursor.y++;
-
-#if 1
-    /* Check if we need to scroll the display up */
-    if(console.cursor.y > (console.dimensions.h/VGA_FONT_HEIGHT) ) {
-      mlc_memcpy(console.fb,
-		 console.fb+(console.dimensions.w*VGA_FONT_HEIGHT*2),
-		 (console.dimensions.w*console.dimensions.h*2) - 
-		 (console.dimensions.w*VGA_FONT_HEIGHT*2) );
-    }
-#endif
-    return;
-  }
-  if(ch == '\r') { console.cursor.x = 0; return; }
-
-  /* !!! Assumes RGB565 */
-
-  for(r=0;r<16;r++) {
-    for(c=0;c<8;c++) {
-      if( (uint8)font8x16[(uint8)ch][r] & (1<<(8-c)) ) {  /* Pixel set */
-	console.fb[(y+r)*console.dimensions.w+x+c] = 0xFFFF;
-      } else { /* Pixel clear */
-	console.fb[(y+r)*console.dimensions.w+x+c] = 0;
-      }
-    }
-  }
-
-  if( console.cursor.x > (console.dimensions.w/VGA_FONT_WIDTH) ) {
-    console.cursor.x = 0; 
-    console.cursor.y++;
-  } else {
-    console.cursor.x += 1;
-  }
-
-  if(ch == '\n')
-    fb_update(console.fb);
 }
 
 void console_puts(volatile char *str) {
@@ -141,7 +95,7 @@ void console_puts(volatile char *str) {
 void console_putsXY(int x,int y,volatile char *str) {
   while(*str != 0) {
     console_blitchar(x,y,*str);
-    x += VGA_FONT_WIDTH;
+    x += FONT_WIDTH;
     str++;
   }
 }
