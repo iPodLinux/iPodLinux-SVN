6a70375a1b53ae109576cd5e5dc7346289d993cc
josh <josh@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2005-12-25 23:17:45 +0000

    Use a different font depending on the screen size.

--- tools/ipodloader2/console.h
+++ tools/ipodloader2/console.h
@@ -2,7 +2,9 @@
 #define _CONSOLE_H_
 
 #include "bootloader.h"
+#include "fontlarge.h"
 #include "fontmedium.h"
+#include "fontsmall.h"
 
 void console_init(uint16 *fb);
 void console_putchar(char ch);
@@ -11,5 +13,8 @@ void console_putsXY(int x,int y,volatile char *str);
 void console_printf (const char *format, ...);
 
 void console_setcolor(uint16 fg,uint16 bg,uint8 transparent);
+
+extern int font_width, font_height;
+extern const uint8 *fontdata;
 
 #endif
