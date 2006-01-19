a44b575f956da5996c30201391744978199e29fe
courtc <ccavin@gmail.com>
2006-01-19 09:26:03 +0000

    ported calc to ttk

--- tools/podzilla2/modules/calc/calc.c
+++ tools/podzilla2/modules/calc/calc.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004 Courtney Cavin
+ * Copyright (C) 2004, 2006 Courtney Cavin
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,200 +18,187 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#define PZ_COMPAT
+#include <math.h>
 #include "pz.h"
 
-static GR_WINDOW_ID calc_wid;
-static GR_GC_ID calc_gc;
+#define DIV  16
+#define MULT 17
+#define SUBT 18
+#define ADD  19
+#define EQL  20
+#define DP   21
 
-int xlocal,ylocal,lastxlocal,lastylocal;
-char mathloc[16];
-double oldnum;
-int numfull, littr;
+#define N_BUTTONS (16 - 1)
+struct _buttons {
+	char *ch;
+	unsigned char math;
+} buttons[] = {
+	{"7", 7}, {"8", 8}, {"9", 9}, {"/", DIV},
+	{"4", 4}, {"5", 5}, {"6", 6}, {"*", MULT},
+	{"1", 1}, {"2", 2}, {"3", 3}, {"-", SUBT},
+	{"0", 0}, {".", DP},{"=", EQL},{"+",ADD}
+};
 
-static int current_calc_item = 0;
-static int last_calc_item;
-char *num[]={"7","8","9","/","4","5","6","*","1","2","3","-","0",".","=","+"};
+static PzModule *module;
+static double number, lastnumber;
+static signed char e;
+static unsigned char op;
+static int cur_button;
 
-static int cxgap = 5, cygap = 4;
-static int cbw = 25, cbh = 16;
+static void draw_button(PzWidget *wid, ttk_surface srf, int row, int col)
+{
+#define XOFF (wid->w / 12)
+#define YOFF (wid->h / 4)
+#define SECW ((wid->w - XOFF*2)/4) 
+#define SECH ((wid->h - YOFF - wid->h/12)/4) 
+#define BPAD ((SECW - ttk_text_width(ttk_textfont, "8"))/4)
+#define BW (SECW - BPAD)
+#define BH (SECH - BPAD)
+	char *text[] = {"button.selected.fg", "button.default.fg",
+		"button.selected.bg", "button.default.bg",
+		"button.selected.border", "button.default.border"};
+	int pos = (row * 4) + col;
+	unsigned char ap = !(pos == cur_button);
+	int x = (XOFF + BPAD/2) + col*(BW + BPAD);
+	int y = (YOFF + BPAD/2) + row*(BH + BPAD);
 
-#define DIV 3
-#define MULT 7
-#define SUB 11
-#define ADD 15
-
-#define CHEIGHT	((cbh*5)+(cygap << 2))
-#define CWIDTH	((cbw << 2)+(cxgap*3))
-#define CXOFF	((screen_info.cols-CWIDTH) >> 1)
-#define CYOFF	(((screen_info.rows-(HEADER_TOPLINE+1))-CHEIGHT) >> 1)
-#define CBYOFF	(CYOFF+cbh+cygap)
-
-static void draw_calc() {
-	GR_SIZE width, height, base;
-
-	GrSetGCForeground(calc_gc, appearance_get_color(CS_FG));
-	GrRect(calc_wid, calc_gc, CXOFF, CYOFF, CWIDTH, cbh);
-	GrGetGCTextSize(calc_gc, mathloc, -1, GR_TFASCII, &width, &height,
-			&base);
-	GrSetGCForeground(calc_gc, appearance_get_color(CS_BG));
-	GrFillRect(calc_wid, calc_gc, CXOFF+1, CYOFF+1, CWIDTH-2, cbh-2);
-	GrSetGCForeground(calc_gc, appearance_get_color(CS_FG));
-	GrText(calc_wid, calc_gc, CXOFF+(CWIDTH-width-4),
-		CYOFF+((cbh/2)-(height/2)), mathloc, -1, GR_TFASCII|GR_TFTOP);
-
-	if(current_calc_item < 0)
-		current_calc_item = 15;
-	else if(current_calc_item >= 16)
-		current_calc_item = 0;
-		
-	xlocal=CXOFF+((cbw+cxgap)*(current_calc_item%4));
-	ylocal=CBYOFF+((cbh+cygap)*(current_calc_item >> 2));
-	lastxlocal=CXOFF+((cbw+cxgap)*(last_calc_item%4));
-	lastylocal=CBYOFF+((cbh+cygap)*(last_calc_item >> 2));
-	
-	GrSetGCForeground(calc_gc, appearance_get_color(CS_BG));
-	GrFillRect(calc_wid, calc_gc, lastxlocal+1, lastylocal+1, cbw-2, cbh-2);
-	GrSetGCForeground(calc_gc, appearance_get_color(CS_FG));
-	GrGetGCTextSize(calc_gc, num[last_calc_item], -1, GR_TFASCII,
-	                &width, &height, &base);
-	GrText(calc_wid, calc_gc,
-	       lastxlocal+((cbw/2)-(width/2)), lastylocal+((cbh/2)-(height/2)),
-	       num[last_calc_item], -1, GR_TFASCII|GR_TFTOP);
-	GrSetGCForeground(calc_gc, appearance_get_color(CS_SELBG));
-	GrFillRect(calc_wid, calc_gc, xlocal, ylocal, cbw, cbh);
-	GrSetGCForeground(calc_gc, appearance_get_color(CS_SELFG));
-	GrGetGCTextSize(calc_gc, num[current_calc_item], -1, GR_TFASCII,
-	                &width, &height, &base);
-	GrText(calc_wid, calc_gc,
-	       xlocal+((cbw/2)-(width/2)), ylocal+((cbh/2)-(height/2)),
-	       num[current_calc_item], -1, GR_TFASCII|GR_TFTOP);
+	ttk_ap_rect(srf, ttk_ap_get(text[ap+4]), x, y, x + BW, y + BH);
+	ttk_ap_fillrect(srf, ttk_ap_get(text[ap+2]), x+1, y+1, x+BW-1, y+BH-1);
+	ttk_text(srf, ttk_textfont, x+(BW-ttk_text_width(ttk_textfont, "8"))/2,
+			y+(BH-ttk_text_height(ttk_textfont))/2,
+			ttk_ap_getx(text[ap])->color, buttons[pos].ch);
 }
 
-void calc_do_math(int pos) {
-	static int opr, tog;
-	if((pos==DIV || pos==MULT || pos==SUB || pos>13)&&mathloc[0]!='\0') {
-		/* convert string to double, store */
-		if(numfull) {
-			if(opr==DIV) /* divide */
-				sprintf(mathloc, "%g", oldnum/atof(mathloc));
-			else if(opr==MULT) /* multiply */
-				sprintf(mathloc, "%g", oldnum*atof(mathloc));
-			else if(opr==SUB) /* subtract */
-				sprintf(mathloc, "%g", oldnum-atof(mathloc));
-			else if(opr==ADD) /* add */
-				sprintf(mathloc, "%g", oldnum+atof(mathloc));
+static void calculator_draw(PzWidget *wid, ttk_surface srf)
+{
+	int row, col;
+	int x = XOFF + BPAD/2;
+	int y = wid->h/12;
+	int xe = x + (wid->w - XOFF*2) - BPAD;
+	char s[24];
+
+	snprintf(s, 24, "%g", number);
+	ttk_rect(srf, x, y, xe, y+BH, ttk_ap_getx("window.fg")->color);
+	ttk_text(srf, ttk_textfont, xe - ttk_text_width(ttk_textfont, s) - 1,
+			y + (BH - ttk_text_height(ttk_textfont))/2,
+			ttk_ap_getx("window.fg")->color, s);
+	
+	for (row = 0; row < 4; row++)
+		for (col = 0; col < 4; col++)
+			draw_button(wid, srf, row, col);
+}
+
+static double lpow(double x, signed char p)
+{
+	double i = x;
+	if (p > 0) while (--p > 0) x *= i;
+	else if (p-- < 0) while (p++ < 0) x /= i;
+	else return 1;
+	return x;
+}
+
+static double operation(double a, unsigned char op, double b)
+{
+	switch (op) {
+	case DIV: return a / b;
+	case MULT:return a * b;
+	case SUBT:return a - b;
+	case ADD: return a + b;
+	}
+	return 0;
+}
+
+static void calculate(int pos)
+{
+	struct _buttons *p = &buttons[pos];
+	static int stump = 0;
+	if (p->math & 16) {
+		switch (p->math) {
+		case DIV:
+		case MULT:
+		case SUBT:
+		case ADD:
+			op = p->math;
+			lastnumber = number;
+			stump = 1;
+			e = 0;
+			break;
+		case EQL:
+			number = operation(lastnumber, op, number);
+			stump = 1;
+			e = 0;
+			break;
+		case DP:
+			if (!e) --e;
+			break;
 		}
-		oldnum = atof(mathloc);
-		opr=pos;
-		numfull=1;
-		tog=1;
-		littr=0;
+	}
+	else if (!stump) {
+		if (!e) number *= 10;
+		number += p->math * lpow(10, e);
+		if (e < 0) --e;
 	}
 	else {
-		if(opr==14&& tog) {
-			numfull=0;
-			mathloc[0]='\0';
-			tog=0;
-		}
-		if(numfull && tog) {
-			mathloc[0]='\0';
-			tog=0;
-		}
-		littr++;
-		/* add number to string */
-		if(littr<16)
-			sprintf(mathloc, "%s%s", mathloc, num[pos]);
+		number = p->math;
+		stump = 0;
 	}
-
 }
 
-static void calc_do_draw() {
-	int i;
-	GR_SIZE width, height, base;
-	pz_draw_header(_("Calculator"));
-	GrSetGCForeground(calc_gc, appearance_get_color(CS_FG));
-	for(i=0; i<=15; i++) {
-		GrGetGCTextSize(calc_gc, num[i], -1, GR_TFASCII,
-		                &width, &height, &base);
-		GrRect(calc_wid, calc_gc,CXOFF+((cbw+cxgap)*(i%4)), 
-		       CBYOFF+((cbh+cygap)*(i/4)), cbw, cbh);
-		GrText(calc_wid, calc_gc,
-		       CXOFF+((cbw+cxgap)*(i%4))+((cbw/2)-(width/2)),
-		       CBYOFF+((cbh+cygap)*(i/4))+((cbh/2)-(height/2)),
-		       num[i], -1, GR_TFASCII|GR_TFTOP);
-	}
-	draw_calc();
-}
+#define BWRAP(x,y,z) if (x < y) x = z
+#define TWRAP(x,y,z) if (x > z) x = y
+#define WRAP(x,y,z) do { BWRAP(x,y,z); TWRAP(x,y,z); } while(0)
 
-static int calc_do_keystroke(GR_EVENT * event) {
-	int ret = 0;
+static int calculator_events(PzEvent *e)
+{
+	int ret = TTK_EV_CLICK;
 
-	switch(event->type) {
-	case GR_EVENT_TYPE_KEY_DOWN:
-		switch (event->keystroke.ch) {
-		case IPOD_BUTTON_ACTION:
-		case '\n':
-			calc_do_math(current_calc_item);
-			draw_calc();
-			ret |= KEY_CLICK;
+	switch(e->type) {
+	case PZ_EVENT_BUTTON_DOWN:
+		switch (e->arg) {
+		case PZ_BUTTON_ACTION:
+			calculate(cur_button);
+			e->wid->dirty = 1;
 			break;
 
-		case IPOD_WHEEL_COUNTERCLOCKWISE:
-			last_calc_item = current_calc_item;
-			current_calc_item--;
-			draw_calc();
-			ret |= KEY_CLICK;
-			break;
-
-		case IPOD_WHEEL_CLOCKWISE:
-			last_calc_item = current_calc_item;
-			current_calc_item++;
-			draw_calc();
-			ret |= KEY_CLICK;
-			break;
-
-		case IPOD_BUTTON_MENU:
-			GrDestroyGC(calc_gc);
-			pz_close_window(calc_wid);
-			ret |= KEY_CLICK;
+		case PZ_BUTTON_MENU:
+			pz_close_window(e->wid->win);
 			break;
 
 		default:
-			ret |= KEY_UNUSED;
+			ret = TTK_EV_UNUSED;
 		}
 		break;
 
+	case PZ_EVENT_SCROLL:
+		cur_button += e->arg;
+		WRAP(cur_button, 0, N_BUTTONS);
+		e->wid->dirty = 1;
+		break;
+
 	default:
-		ret |= EVENT_UNUSED;
+		ret = TTK_EV_UNUSED;
 		break;
 	}
 	return ret;
 }
 
-void new_calc_window() {
-	calc_gc = pz_get_gc(1);
-	GrSetGCUseBackground(calc_gc, GR_FALSE);
-	GrSetGCForeground(calc_gc, appearance_get_color(CS_FG));
+static PzWindow *new_calculator()
+{
+	PzWindow *ret;
+	number = lastnumber = 0;
+	e = 0;
+	cur_button = 0;
 
-	mathloc[0]='\0';
-	numfull = 0;
-	littr = 0;
+	ret = pz_new_window(_("Calculator"), PZ_WINDOW_NORMAL);
+	pz_add_widget(ret, calculator_draw, calculator_events);
 
-	if (screen_info.cols < 160) { /* mini */
-		cxgap = 3; cygap = 2;
-		cbw = 22; cbh = 15;
-	}
-
-	calc_wid = pz_new_window(0, HEADER_TOPLINE + 1, screen_info.cols,
-			screen_info.rows - (HEADER_TOPLINE + 1), calc_do_draw,
-			calc_do_keystroke);
-	GrSetWindowBackgroundColor(calc_wid, appearance_get_color(CS_BG));
-
-	GrSelectEvents(calc_wid, GR_EVENT_MASK_EXPOSURE |
-			GR_EVENT_MASK_KEY_UP | GR_EVENT_MASK_KEY_DOWN);
-
-	GrMapWindow(calc_wid);
+	return pz_finish_window(ret);
 }
 
-PZ_SIMPLE_MOD ("calc", new_calc_window, "/Extras/Calculator")
+static void init_calculator()
+{
+	module = pz_register_module("calc", NULL);
+	pz_menu_add_action("/Extras/Calculator", new_calculator);
+}
+
+PZ_MOD_INIT(init_calculator)
