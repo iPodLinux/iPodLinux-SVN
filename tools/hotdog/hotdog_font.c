45b61da11e088574c13c1eb996fc6b5c7de00156
oremanj <oremanj@rwcr.net>
2006-01-21 22:13:51 +0000

    Added a few font-loading functions. Nothing to draw 'em yet, though.

--- tools/hotdog/hotdog_font.c
+++ tools/hotdog/hotdog_font.c
@@ -1,118 +1,266 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
+#include <string.h>
+#include <netinet/in.h>
 
 #include "hotdog.h"
 #include "hotdog_font.h"
 
-#include <ft2build.h>
-#include <freetype/freetype.h>
+#define HDF_MAGIC 0x676f6448
 
-#if 0
-static void HD_Font_RenderGlyph(hd_font *font, FT_Bitmap *ftmap, int32 x,int32 y, int32 w, int32 h ) {
-  int32 fx,fy;
-  uint8 luma;
-  
-  if(x<0)x=0; // Silly FT2...
+/* The HDF header. All fields are stored big-endian. */
 
-  for(fy=0;fy<ftmap->rows;fy++) {
-    for(fx=0;fx<ftmap->width;fx++) {
-      luma = (*(uint8 *)(ftmap->buffer + (fy*ftmap->pitch + fx)))&0xFF;
-
-      font->argb[ (fy+y) * font->w + (fx+x) ] = (luma<<24) | (luma<<16) | (luma<<8) | luma;
-    }
-  }
-}
-#endif
-
-hd_object *HD_Font_Create(char *font,uint32 height,char *text) {
-#if 0
-  hd_object *ret;
-  hd_font *fsub;
-  FT_Library library;
-  FT_Face    face;
-  int        error;
-  int32      xsize,n,x;
-
-  ret = HD_New_Object();
-  ret->type = HD_TYPE_FONT;
-  ret->sub.font = fsub = (hd_font *)malloc( sizeof(hd_font) );
-  assert(font != NULL);
-
-  error = FT_Init_FreeType( &library );
-  assert(error == 0);
-
-  error = FT_New_Face( library, font, 0, &face );
-  assert(error == 0);
-
-  error = FT_Set_Pixel_Sizes( face, 0, height );
-  assert(error == 0);
-
-  xsize = 0;
-  for(n=0;n<strlen(text);n++) {
-    FT_UInt glyph_index;
-
-    glyph_index = FT_Get_Char_Index( face, text[n] );
-
-    error = FT_Load_Glyph( face, glyph_index, FT_LOAD_DEFAULT );
-    assert( error == 0 );
-
-    error = FT_Render_Glyph( face->glyph, ft_render_mode_normal );
-    assert( error == 0 );
-
-    xsize += face->glyph->advance.x >> 6;
-  }
-  xsize++;
-
-  fsub->argb = (uint32 *)malloc( xsize * height * 4 );
-  assert(fsub->argb != NULL);
-
-  memset(fsub->argb,0,xsize * height * 4);
-  ret->natw = fsub->w = xsize;
-  ret->nath = fsub->h = height;
-
-  x = 0;
-  for(n=0;n<strlen(text);n++) {
-    FT_UInt glyph_index;
-
-    glyph_index = FT_Get_Char_Index( face, text[n] );
-
-    error = FT_Load_Glyph( face, glyph_index, FT_LOAD_DEFAULT );
-    assert( error == 0 );
-
-    error = FT_Render_Glyph( face->glyph, ft_render_mode_normal );
-    assert( error == 0 );
-
-    HD_Font_RenderGlyph( fsub, &face->glyph->bitmap,
-			 x + face->glyph->bitmap_left,(height - face->glyph->bitmap_top),
-			 face->glyph->advance.x >> 6,
-			 height );
-
-    x += face->glyph->advance.x >> 6;
-  }
-
-  ret->destroy = HD_Font_Destroy;
-  ret->render = HD_Font_Render;
-
-  return(ret);
-#endif
-  return 0;
-}
-
-void     HD_Font_Destroy(hd_object *obj) {
-#if 0
-  if( obj->sub.font->argb != NULL) free(obj->sub.font->argb);
-  free (obj->sub.font);
-#endif
-}
-
-void HD_Font_Render (hd_engine *eng, hd_object *obj, int x, int y, int w, int h) 
+typedef struct HDF_header
 {
-#if 0
-    HD_ScaleBlendClip (obj->sub.font->argb, obj->sub.font->w, obj->sub.font->h,
-                       x, y, w, h,
-                       eng->buffer, eng->screen.width, eng->screen.height,
-                       obj->x, obj->y, obj->w, obj->h);
-#endif
+    uint32  magic;              /* 'Hdog' = 0x676f6448 */
+    uint16  nchars;             /* number of characters in the font */
+    uint16  firstchar;          /* first character */
+    uint8   bpp;                /* bits per pixel: 1, 8, or 32 */
+    uint8   bitstype;           /* HD_FONT_PITCHED or HD_FONT_CLUMPED */
+    uint16  height;             /* height of the font, height of the data surface */
+    uint32  pitch;              /* width in bytes of a scanline in the data surface.
+                                   Only valid for PITCHED fonts. */
+    uint32  nbytes;             /* Number of bytes of character data to read */
+} __attribute__ ((packed)) HDF_header;
+
+/* Following the header are <nchars> 32-bit BE values indicating the offset
+ * in pixels into the data surface.
+ * (The first one is char <firstchar>, second is char <firstchar>+1, etc.)
+ *
+ * Following that are <nchars> bytes indicating the *width* of
+ * each character, indexed in the same way.
+ *
+ * Following that is the data surface.
+ */
+
+/* 1-bpp fonts look like
+ *
+ * MSb, byte 0          LSb  MSb, byte 1          LSb
+ *  |                    |    |                    |
+ *  v                    v    v                    v
+ *  0  1  2  3  4  5  6  7    8  9 10 11 12 13 14 15    ...
+ *
+ * That is, the bits are arranged in bytes, MSb first. The numbers
+ * indicate bit positions (0=first bit, 1=second, ...).
+ */
+
+/* 32-bit fonts are stored in AA RR GG BB byte order (ARGB big-endian),
+ * and cannot be colored differently from their color in the file. 
+ */
+
+hd_font *HD_Font_LoadHDF (const char *filename) 
+{
+    int i;
+    FILE *fp = fopen (filename, "rb");
+    if (!fp) return 0;
+    
+    HDF_header hdr;
+    fread (&hdr, 20, 1, fp);
+
+    hdr.magic     = ntohl (hdr.magic);
+    hdr.nchars    = ntohs (hdr.nchars);
+    hdr.firstchar = ntohs (hdr.firstchar);
+    hdr.height    = ntohs (hdr.height);
+    hdr.pitch     = ntohl (hdr.pitch);
+    hdr.nbytes    = ntohl (hdr.nbytes);
+
+    if (hdr.magic != HDF_MAGIC || (hdr.bpp != 1 && hdr.bpp != 8 && hdr.bpp == 32)) {
+        fclose (fp);
+        return 0;
+    }
+
+    hd_font *font = malloc (sizeof(hd_font));
+    font->bitstype    = hdr.bitstype;
+    font->pitch       = hdr.pitch;
+    font->h           = hdr.height;
+    font->bpp         = hdr.bpp;
+    font->firstchar   = hdr.firstchar;
+    font->nchars      = hdr.nchars;
+    font->defaultchar = 0;
+    font->pixbytes    = hdr.nbytes;
+    font->offset      = malloc (4 * font->nchars);
+    font->width       = malloc (font->nchars);
+    font->pixels      = malloc (font->pixbytes);
+    
+    if (!font->offset || !font->width || !font->pixels) {
+        if (font->offset) free (font->offset);
+        if (font->width)  free (font->width);
+        if (font->pixels) free (font->pixels);
+        free (font);
+        return 0;
+    }
+
+    fread (font->offset, 4, font->nchars, fp);
+    for (i = 0; i < font->nchars; i++)
+        font->offset[i] = ntohl (font->offset[i]);
+
+    fread (font->width, 1, font->nchars, fp);
+
+    fread (font->pixels, font->pixbytes, 1, fp);
+    if (font->bpp == 32) {
+        uint32 *pp;
+        for (pp = font->pixels; pp < (uint32 *)((char *)font->pixels + font->pixbytes); pp++)
+            *pp = ntohl (*pp);
+    }
+
+    return font;
 }
 
+int HD_Font_SaveHDF (hd_font *font, const char *filename) 
+{
+    int i;
+    FILE *fp = fopen (filename, "wb");
+    if (!fp) return -1;
+    
+    HDF_header hdr;
+    hdr.magic     = htonl (HDF_MAGIC);
+    hdr.nchars    = htons (font->nchars);
+    hdr.firstchar = htons (font->firstchar);
+    hdr.bpp       = font->bpp;
+    hdr.bitstype  = font->bitstype;
+    hdr.height    = htons (font->h);
+    hdr.pitch     = htonl (font->pitch);
+    hdr.nbytes    = htonl (font->pixbytes);
+
+    fwrite (&hdr, 16, 1, fp);
+
+    for (i = 0; i < font->nchars; i++)
+        font->offset[i] = htonl (font->offset[i]);
+
+    fwrite (font->offset, 4, font->nchars, fp);
+
+    for (i = 0; i < font->nchars; i++)
+        font->offset[i] = ntohl (font->offset[i]);
+    
+    fwrite (font->width, 1, font->nchars, fp);
+    
+    if (font->bpp == 32) {
+        uint32 *pp;
+        for (pp = font->pixels; pp < (uint32 *)((char *)font->pixels + font->pixbytes); pp++)
+            *pp = htonl (*pp);
+    }
+
+    fwrite (font->pixels, font->pixbytes, 1, fp);
+    
+    if (font->bpp == 32) {
+        uint32 *pp;
+        for (pp = font->pixels; pp < (uint32 *)((char *)font->pixels + font->pixbytes); pp++)
+            *pp = ntohl (*pp);
+    }
+
+    fclose (fp);
+
+    return 0;
+}
+
+
+/* Utility functions for LoadFNT and LoadPCF - read big-endian datums. */
+static int read16 (FILE *fp, uint16 *sp)
+{
+    int c;
+    unsigned short s = 0;
+    if ((c = getc (fp)) == EOF) return 0;
+    s |= (c & 0xff);
+    if ((c = getc (fp)) == EOF) return 0;
+    s |= (c & 0xff) << 8;
+    *sp = s;
+    return 1;
+}
+static int read32 (FILE *fp, uint32 *lp)
+{
+    int c;
+    unsigned long l = 0;
+    if ((c = getc (fp)) == EOF) return 0;
+    l |= (c & 0xff);
+    if ((c = getc (fp)) == EOF) return 0;
+    l |= (c & 0xff) << 8;
+    if ((c = getc (fp)) == EOF) return 0;
+    l |= (c & 0xff) << 16;
+    if ((c = getc (fp)) == EOF) return 0;
+    l |= (c & 0xff) << 24;
+    *lp = l;
+    return 1;
+}
+static int readstr (FILE *fp, char *buf, int count)
+{
+    return fread (buf, 1, count, fp);
+}
+
+hd_font *HD_Font_LoadFNT (const char *filename) 
+{
+    int i;
+    FILE *fp = fopen (filename, "rb");
+    if (!fp) return 0;
+    
+    uint16 maxwidth, height, ascent, pad;
+    uint32 firstchar, defaultchar, nchars;
+    uint32 nbits, noffset, nwidth;
+    char version[5];
+    
+    memset (version, 0, 5);
+    if (readstr (fp, version, 4) != 4)
+        goto errout;
+    if (strcmp (version, "RB11") != 0)
+        goto errout;
+    
+    fseek (fp, 0x140, SEEK_CUR); // skip name and copyright
+
+    if (!read16 (fp, &maxwidth)) goto errout;
+    if (!read16 (fp, &height)) goto errout;
+    if (!read16 (fp, &ascent)) goto errout;
+    if (!read16 (fp, &pad)) goto errout;
+    if (!read32 (fp, &firstchar)) goto errout;
+    if (!read32 (fp, &defaultchar)) goto errout;
+    if (!read32 (fp, &nchars)) goto errout;
+    if (!read32 (fp, &nbits)) goto errout; // nbits = number of *words* of bmap data
+    if (!read32 (fp, &noffset)) goto errout;
+    if (!read32 (fp, &nwidth)) goto errout;
+    
+    hd_font *font     = malloc (sizeof(hd_font));
+    font->bitstype    = HD_FONT_CLUMPED;
+    font->pitch       = 2;
+    font->h           = height;
+    font->bpp         = 1;
+    font->firstchar   = firstchar;
+    font->nchars      = nchars;
+    font->defaultchar = defaultchar;
+    font->pixels      = malloc (font->pixbytes = 2 * nbits);
+    font->offset      = malloc (4 * nchars);
+    font->width       = malloc (nchars);
+
+    if (!font->pixels || !font->offset || !font->width) {
+        goto errfree;
+    }
+
+    if (nbits & 1) nbits++;
+    fread (font->pixels, 2, nbits, fp);
+
+    if (!noffset) goto errfree;
+
+    if (noffset > nchars) noffset = nchars;
+    for (i = 0; i < noffset; i++) {
+        if (!read32 (fp, font->offset + i))
+            goto errfree;
+    }
+    if (noffset < nchars) printf ("warning: too few offset data\n");
+
+    if (nwidth) {
+        if (nwidth > nchars) nwidth = nchars;
+        fread (font->width, 1, nwidth, fp);
+        if (nwidth < nchars) memset (font->width + nwidth, maxwidth, nchars - nwidth);
+    } else {
+        memset (font->width, maxwidth, nchars);
+    }
+
+    fclose (fp);
+    return font;
+    
+ errfree:
+    if (font->offset) free (font->offset);
+    if (font->width) free (font->width);
+    if (font->pixels) free (font->pixels);
+    free (font);
+ errout:
+    fclose (fp);
+    return 0;
+}
--- tools/hotdog/hotdog_font.h
+++ tools/hotdog/hotdog_font.h
@@ -3,8 +3,8 @@
 
 #include "hotdog.h"
 
-hd_object *HD_Font_Create(char *font,uint32 height,char *text);
-void       HD_Font_Destroy(hd_object *obj);
-void       HD_Font_Render(hd_engine *eng,hd_object *obj, int x, int y, int w, int h);
+hd_font *HD_Font_LoadHDF (const char *filename);
+int      HD_Font_SaveHDF (hd_font *font, const char *filename);
+hd_font *HD_Font_LoadFNT (const char *filename);
 
 #endif
