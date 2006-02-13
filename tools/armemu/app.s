81dd681c555391beda531e23ebedd68905d5e375
josh <josh@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2006-02-13 01:35:26 +0000

    Added some debug MMIOs, and made app.s use them.

--- tools/armemu/app.s
+++ tools/armemu/app.s
@@ -17,10 +17,20 @@ IVEC_fiq:
 start:	mov	r0, #0x40
 	mov	r1, #0x80
 loop:	cmp	r0, r1
-	bhi	.
+	bhi	done
 	add	r0, r0, #2
 	add	r1, r1, #1
 	b	loop
+done:	mov	r2, #0x70000000
+	ldr	r3, [r2, #-16]
+	mov	r3, r3, ror #16
+	str	r3, [r2, #-4]
+	adr	r3, string
+	str	r3, [r2, #-12]
+	ldr	r3, =1000000
+	str	r3, [r2, #-8]
+	b	.
+string:	.asciz	"Hello, world!"
 
 @ Local Variables:
 @ indent-tabs-mode: t
