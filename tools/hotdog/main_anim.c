7a41e2a8753d0db2fc999a1cba510ad265933b7f
courtc <ccavin@gmail.com>
2006-02-17 12:07:18 +0000

    added input for rotating hd-demo cleaned up indention, all tabs; modify your text editor settings to suit your needs

--- tools/hotdog/main_anim.c
+++ tools/hotdog/main_anim.c
@@ -24,7 +24,7 @@
  * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
- 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -34,171 +34,302 @@
 
 #include "hotdog.h"
 
-#include "SDL.h"
-
 #define WIDTH  320
 #define HEIGHT 240
 
-#ifndef IPOD
-SDL_Surface *screen;
-
-uint32 GetTimeMillis(void) {
-  uint32 millis;
-
-  millis = SDL_GetTicks();
-
-  return(millis);
-}
-#else
-uint32 *screen;
-#endif
-
 #ifdef IPOD
+#include <termios.h> 
+#include <sys/time.h>
+
+uint32 *screen;
+
+static struct termios stored_settings; 
+
+void set_keypress(void)
+{
+	struct termios new_settings;
+	tcgetattr(0,&stored_settings);
+	new_settings = stored_settings;
+	new_settings.c_lflag &= (~ICANON);
+	new_settings.c_cc[VTIME] = 0;
+	tcgetattr(0,&stored_settings);
+	new_settings.c_cc[VMIN] = 1;
+	tcsetattr(0,TCSANOW,&new_settings);
+}
+
+void reset_keypress(void)
+{
+	tcsetattr(0,TCSANOW,&stored_settings);
+}
+
 extern void _HD_ARM_Update5G (uint16 *fb, int x, int y, int w, int h);
 static void update (hd_engine *eng, int x, int y, int w, int h)
 {
-    _HD_ARM_Update5G (eng->screen.framebuffer, x, y, w, h);
+	_HD_ARM_Update5G (eng->screen.framebuffer, x, y, w, h);
 }
-#else
-static void update (hd_engine *eng, int x, int y, int w, int h) 
+
+uint32 GetTimeMillis(void)
 {
-    SDL_UpdateRect (SDL_GetVideoSurface(), x, y, w, h);
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return (uint32)((tv.tv_sec % 0xffff) * 1000 + tv.tv_usec / 1000);
+}
+
+#else
+#include "SDL.h"
+
+SDL_Surface *screen;
+
+uint32 GetTimeMillis(void)
+{ return (uint32)SDL_GetTicks(); }
+
+static void update (hd_engine *eng, int x, int y, int w, int h)
+{
+	SDL_UpdateRect (SDL_GetVideoSurface(), x, y, w, h);
 }
 #endif
 
+typedef struct circle_object {
+	hd_object *object;
+	int32 position;
+} circle_object;
+
+#define check_pending() add_pending(0)
+static int add_pending(int dir)
+{
+	static unsigned char s;
+	static struct _pends {
+		char dir;
+		int32 time;
+	} pends[2];
+
+	if (!dir) {
+		int ra,rb;
+	       	ra = GetTimeMillis()-pends[0].time;
+		rb = GetTimeMillis()-pends[1].time;
+		return (ra > 350 && rb > 350) ? 0 : pends[(ra > rb)].dir;
+	}
+
+	s = !s;
+	pends[s].dir = (char)dir;
+	pends[s].time = GetTimeMillis();
+	return 0;
+}
+	
+static void circle_rotate(circle_object *circle, int dir)
+{
+	HD_AnimateCircle(circle->object, 80, 50, 50, (50 << 16) /
+			circle->object->w, (70 << 16) / circle->object->w,
+			circle->position, dir*1024, 25);
+	if (dir < 0) {
+		circle->position = (circle->position - 1024);
+		if (circle->position < 0) circle->position += 4096;
+	}
+	if (dir > 0) circle->position = (circle->position + 1024) % 4096;
+}
+
 int main(int argc, char *argv[]) {
-  uint32  done = 0;
-	uint16* pFB=0;
+	uint32 done = 0;
+	char ch;
 	hd_engine *engine;
-	hd_object *obj[5];
-	double f = 0.0;
+	circle_object obj[5];
 
 #ifndef IPOD
-  if( SDL_Init(SDL_INIT_VIDEO) < 0 ) {
-    fprintf(stderr,"Unable to init SDL: %s\n",SDL_GetError());
-    exit(1);
-  }
-  atexit(SDL_Quit);
-  
-  screen = SDL_SetVideoMode(WIDTH,HEIGHT,16,SDL_SWSURFACE);
-  if(screen == NULL) {
-    fprintf(stderr,"Unable to init SDL video: %s\n",SDL_GetError());
-    exit(1);
-  }
-  engine = HD_Initialize (WIDTH, HEIGHT, 16, screen->pixels, update);
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		fprintf(stderr,"Unable to init SDL: %s\n",SDL_GetError());
+		exit(1);
+	}
+	atexit(SDL_Quit);
+
+	screen = SDL_SetVideoMode(WIDTH,HEIGHT,16,SDL_SWSURFACE);
+	if (screen == NULL) {
+		fprintf(stderr,"Unable to init SDL video: %s\n",SDL_GetError());
+		exit(1);
+	}
+	engine = HD_Initialize (WIDTH, HEIGHT, 16, screen->pixels, update);
 #define IMGPREFIX ""
 #else
-  screen = malloc (WIDTH * HEIGHT * 2);
-  engine = HD_Initialize (WIDTH, HEIGHT, 16, screen, update);
+	int t = 0;
+
+	screen = malloc (WIDTH * HEIGHT * 2);
+	engine = HD_Initialize (WIDTH, HEIGHT, 16, screen, update);
 #define IMGPREFIX "/mnt/"
 #endif
 
-        obj[4]    = HD_PNG_Create (IMGPREFIX "bg.png");
-        obj[4]->x = 0;
-        obj[4]->y = 0;
-        obj[4]->w = 220;
-        obj[4]->h = 176;
+	obj[4].object    = HD_PNG_Create (IMGPREFIX "bg.png");
+	obj[4].object->x = 0;
+	obj[4].object->y = 0;
+	obj[4].object->w = 220;
+	obj[4].object->h = 176;
 
-        obj[0]    = HD_PNG_Create (IMGPREFIX "photos.png");
-        obj[0]->x = 0;
-        obj[0]->y = 0;
-        obj[0]->w = 75;
-        obj[0]->h = 150;
+	obj[0].object    = HD_PNG_Create (IMGPREFIX "photos.png");
+	obj[0].object->x = 0;
+	obj[0].object->y = 0;
+	obj[0].object->w = 75;
+	obj[0].object->h = 150;
 
-        obj[1]    = HD_PNG_Create (IMGPREFIX "music.png");
-        obj[1]->x = 0;
-        obj[1]->y = 0;
-        obj[1]->w = 75;
-        obj[1]->h = 150;
+	obj[1].object    = HD_PNG_Create (IMGPREFIX "music.png");
+	obj[1].object->x = 0;
+	obj[1].object->y = 0;
+	obj[1].object->w = 75;
+	obj[1].object->h = 150;
 
-        obj[2]    = HD_PNG_Create (IMGPREFIX "dvd.png");
-        obj[2]->x = 0;
-        obj[2]->y = 0;
-        obj[2]->w = 75;
-        obj[2]->h = 150;
+	obj[2].object    = HD_PNG_Create (IMGPREFIX "dvd.png");
+	obj[2].object->x = 0;
+	obj[2].object->y = 0;
+	obj[2].object->w = 75;
+	obj[2].object->h = 150;
 
-        obj[3]    = HD_PNG_Create (IMGPREFIX "movies.png");
-        obj[3]->x = 0;
-        obj[3]->y = 0;
-        obj[3]->w = 75;
-        obj[3]->h = 150;
+	obj[3].object    = HD_PNG_Create (IMGPREFIX "movies.png");
+	obj[3].object->x = 0;
+	obj[3].object->y = 0;
+	obj[3].object->w = 75;
+	obj[3].object->h = 150;
 
-	HD_Register(engine,obj[4]);
-	HD_Register(engine,obj[0]);
-	HD_Register(engine,obj[1]);
-	HD_Register(engine,obj[2]);
-	HD_Register(engine,obj[3]);
+	HD_Register(engine,obj[4].object);
+	HD_Register(engine,obj[0].object);
+	HD_Register(engine,obj[1].object);
+	HD_Register(engine,obj[2].object);
+	HD_Register(engine,obj[3].object);
 
-        HD_AnimateCircle (obj[0], 80, 50, 50, (50 << 16) / obj[0]->w, (70 << 16) / obj[0]->w,    0, 4096, -100);
-        HD_AnimateCircle (obj[1], 80, 50, 50, (50 << 16) / obj[1]->w, (70 << 16) / obj[1]->w, 1024, 4096, -100);
-        HD_AnimateCircle (obj[2], 80, 50, 50, (50 << 16) / obj[2]->w, (70 << 16) / obj[2]->w, 2048, 4096, -100);
-        HD_AnimateCircle (obj[3], 80, 50, 50, (50 << 16) / obj[3]->w, (70 << 16) / obj[3]->w, 3072, 4096, -100);
+	obj[0].position = 0;
+	circle_rotate(&obj[0], 1);
+	obj[1].position = 1024;
+	circle_rotate(&obj[1], 1);
+	obj[2].position = 2048;
+	circle_rotate(&obj[2], 1);
+	obj[3].position = 3072;
+	circle_rotate(&obj[3], 1);
 
 #ifdef IPOD
-        uint32 srtc = *(volatile uint32 *)0x60005010;
-        int t = 0;
+	uint32 srtc = *(volatile uint32 *)0x60005010;
+
+	set_keypress();
+	fd_set rd;
+	struct timeval tv;
+	int n;
+
 #endif
 
-  while(!done) {
+	while(!done) {
+		if ((ch = check_pending()) && !obj[0].object->animating) {
+			circle_rotate(&obj[0], ch);
+			circle_rotate(&obj[1], ch);
+			circle_rotate(&obj[2], ch);
+			circle_rotate(&obj[3], ch);
+		}
 #ifndef IPOD
-    SDL_Event event;
+		SDL_Event event;
 
-    while(SDL_PollEvent(&event)) {
-      switch(event.type) {
-      case SDL_KEYDOWN:
-	  switch(event.key.keysym.sym) {
-	  case SDLK_SPACE:
-	    break;
-	  case SDLK_BACKSPACE:
-	    break;
-	  case SDLK_DOWN:
-	    break;
-	  case SDLK_UP:
-	    break;
-	  case SDLK_LEFT:
-	    break;
-	  case SDLK_RIGHT:
-	    break;
-	  case SDLK_ESCAPE:
-	    done = 1;
-	    break;
-          default:
-            break;
-	  }
-	break;
-      case SDL_QUIT:
-	return(0);
-	break;
-      default:
-	break;
-      }
-    }
+		while(SDL_PollEvent(&event)) {
+			switch(event.type) {
+			case SDL_KEYDOWN:
+				switch(event.key.keysym.sym) {
+				case SDLK_SPACE:
+				case SDLK_BACKSPACE:
+				case SDLK_DOWN:
+				case SDLK_UP:
+					break;
+				case SDLK_LEFT:
+					if (obj[0].object->animating) {
+						add_pending(1);
+						break;
+					}
+					circle_rotate(&obj[0], 1);
+					circle_rotate(&obj[1], 1);
+					circle_rotate(&obj[2], 1);
+					circle_rotate(&obj[3], 1);
+					break;
+				case SDLK_RIGHT:
+					if (obj[0].object->animating) {
+						add_pending(-1);
+						break;
+					}
+					circle_rotate(&obj[0], -1);
+					circle_rotate(&obj[1], -1);
+					circle_rotate(&obj[2], -1);
+					circle_rotate(&obj[3], -1);
+					break;
+				case SDLK_ESCAPE:
+					done = 1;
+					break;
+				default:
+					break;
+				}
+				break;
+			case SDL_QUIT:
+				return(0);
+				break;
+			default:
+				break;
+			}
+		}
 
-    SDL_Delay (30);
+		SDL_Delay (30);
 #else
-    t++;
-    if (t >= 200) break;
+		FD_ZERO(&rd);
+		FD_SET(0, &rd);
+
+		tv.tv_sec = 0;
+		tv.tv_usec = 100;
+
+		n = select(0+1, &rd, NULL, NULL, &tv);
+		if (FD_ISSET(0, &rd) && (n > 0)) {
+			read(0, &ch, 1);
+			switch(ch) {
+			case 'm':
+				done = 1;
+				break;
+			case 'r':
+				if (obj[0].object->animating) {
+					add_pending(1);
+					break;
+				}
+				circle_rotate(&obj[0], 1);
+				circle_rotate(&obj[1], 1);
+				circle_rotate(&obj[2], 1);
+				circle_rotate(&obj[3], 1);
+				break;
+			case 'l':
+				if (obj[0].object->animating) {
+					add_pending(-1);
+					break;
+				}
+				circle_rotate(&obj[0], -1);
+				circle_rotate(&obj[1], -1);
+				circle_rotate(&obj[2], -1);
+				circle_rotate(&obj[3], -1);
+				break;
+			case 'w':
+			case 'f':
+			case 'd':
+			default:
+				break;
+			}
+		}
+		t++;
 #endif
-    HD_Animate (engine);
+		HD_Animate (engine);
 
 #ifndef IPOD
-    if( SDL_MUSTLOCK(screen) ) 
-      SDL_LockSurface(screen);
+		if( SDL_MUSTLOCK(screen) )
+			SDL_LockSurface(screen);
 #endif
 
 		HD_Render(engine);
 
 #ifndef IPOD
-    if( SDL_MUSTLOCK(screen) ) 
-      SDL_UnlockSurface(screen);
+		if( SDL_MUSTLOCK(screen) )
+			SDL_UnlockSurface(screen);
 #endif
-  }
+	}
 #ifdef IPOD
-  uint32 ertc = *(volatile uint32 *)0x60005010;
-  printf ("%d frames in %d microseconds = %d.%02d frames/sec\n",
-          t, ertc - srtc, 1000000 * t / (ertc - srtc),
-          (1000000 * t / ((ertc - srtc) / 100)) % 100);
-  sleep (5);
+	uint32 ertc = *(volatile uint32 *)0x60005010;
+	printf ("%d frames in %d microseconds = %d.%02d frames/sec\n",
+		t, ertc - srtc, 1000000 * t / (ertc - srtc),
+		(1000000 * t / ((ertc - srtc) / 100)) % 100);
+	sleep (5);
+	reset_keypress();
 #endif
-  return(0);
+	return(0);
 }
