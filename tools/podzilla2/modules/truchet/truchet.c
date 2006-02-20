010b290f5024982c47bef939caf3b231baa3cb8b
yorgle <yorgle@gmail.com>
2006-02-20 20:16:59 +0000

    I got bored, so I added right-triangle patterns in.  it looks purty. ref: http://mathworld.wolfram.com/TruchetTiling.html

--- tools/podzilla2/modules/truchet/truchet.c
+++ tools/podzilla2/modules/truchet/truchet.c
@@ -69,43 +69,61 @@ static void twiddle_buffer()
 void draw_truchet( PzWidget *widget, ttk_surface srf )
 {
 	int x,y;
+	int modulo=1;
 	unsigned short slash1[] = {
-		0x0080, 0x0040, 0x0020, 0x0010,
-		0x0008, 0x0004, 0x0002, 0x0001,
-		0x8000, 0x4000, 0x2000, 0x1000,
-		0x0800, 0x0400, 0x0200, 0x0100,
+		0x0080, 0x0040, 0x0020, 0x0010, 0x0008, 0x0004, 0x0002, 0x0001,
+		0x8000, 0x4000, 0x2000, 0x1000, 0x0800, 0x0400, 0x0200, 0x0100,
 	};
 
 	unsigned short slash2[] = {
-		0x0100, 0x0200, 0x0400, 0x0800,
-		0x1000, 0x2000, 0x4000, 0x8000,
-		0x0001, 0x0002, 0x0004, 0x0008,
-		0x0010, 0x0020, 0x0040, 0x0080,
+		0x0100, 0x0200, 0x0400, 0x0800, 0x1000, 0x2000, 0x4000, 0x8000,
+		0x0001, 0x0002, 0x0004, 0x0008, 0x0010, 0x0020, 0x0040, 0x0080,
 	};
 
 	unsigned short curve1[] = {
-		0x0180, 0x0180, 0x01c0, 0x01c0,
-		0x00e0, 0x00f0, 0x007c, 0xf03f,
-		0xfc0f, 0x3e00, 0x0f00, 0x0700,
-		0x0380, 0x0380, 0x0180, 0x0180,
+		0x0180, 0x0180, 0x01c0, 0x01c0, 0x00e0, 0x00f0, 0x007c, 0xf03f,
+		0xfc0f, 0x3e00, 0x0f00, 0x0700, 0x0380, 0x0380, 0x0180, 0x0180,
 	};
 
 	unsigned short curve2[] = {
-		0x0180, 0x0180, 0x0380, 0x0380,
-		0x0700, 0x0f00, 0x3e00, 0xfc0f,
-		0xf03f, 0x003c, 0x00f0, 0x00e0,
-		0x01c0, 0x01c0, 0x0180, 0x0180,
-	    
+		0x0180, 0x0180, 0x0380, 0x0380, 0x0700, 0x0f00, 0x3e00, 0xfc0f,
+		0xf03f, 0x003c, 0x00f0, 0x00e0, 0x01c0, 0x01c0, 0x0180, 0x0180,
+	};
+
+	unsigned short sl1[] = {
+		0x0001, 0x0003, 0x0007, 0x000f, 0x001f, 0x003f, 0x007f, 0x00ff,
+		0x01ff, 0x03ff, 0x07ff, 0x0fff, 0x1fff, 0x3fff, 0x7fff, 0xffff,
+	};
+
+	unsigned short sl2[] = {
+		0xffff, 0x7fff, 0x3fff, 0x1fff, 0x0fff, 0x07ff, 0x03ff, 0x01ff,
+		0x00ff, 0x007f, 0x003f, 0x001f, 0x000f, 0x0007, 0x0003, 0x0001,
+	};
+
+	unsigned short sl3[] = {
+		0xffff, 0xfffe, 0xfffc, 0xfff8,
+		0xfff0, 0xffe0, 0xffc0, 0xff80,
+		0xff00, 0xfe00, 0xfc00, 0xf800,
+		0xf000, 0xe000, 0xc000, 0x8000,
+	};
+
+	unsigned short sl4[] = {
+		0x8000, 0xc000, 0xe000, 0xf000,
+		0xf800, 0xfc00, 0xfe00, 0xff00,
+		0xff80, 0xffc0, 0xffe0, 0xfff0,
+		0xfff8, 0xfffc, 0xfffe, 0xffff,
 	};
 
 	unsigned short *tile1 = curve1;
 	unsigned short *tile2 = curve2;
+	unsigned short *tile3 = curve1;
+	unsigned short *tile4 = curve2;
 
 	ttk_gc gc = ttk_new_gc();
 	TWindow *pixmap = malloc (sizeof(TWindow)); pixmap->srf = srf;
 
 	/* deal with user input */
-	if( tglob.clicks > 3 ) tglob.clicks = 0;
+	if( tglob.clicks > 5 ) tglob.clicks = 0;
 
 	if( tglob.clicks & 1 ) {
 		tglob.bg = ttk_ap_get( "window.bg" )->color;
@@ -114,23 +132,45 @@ void draw_truchet( PzWidget *widget, ttk_surface srf )
 		tglob.bg = ttk_ap_get( "window.fg" )->color;
 		tglob.fg = ttk_ap_get( "window.bg" )->color;
 	}
-
-	if( tglob.clicks & 2 ) {
-		tile1 = slash1;
-		tile2 = slash2;
-	}
 	tglob.border = ttk_ap_get( "window.border" )->color;
 
+	/* clicks & 1 => color */
+	/* clicks >> 1 => style */
+	switch( (tglob.clicks >> 1) & 3 ){
+	case( 0 ):
+		tile1 = curve1;
+		tile2 = curve2;
+		break;
+
+	case( 1 ):
+		tile1 = slash1;
+		tile2 = slash2;
+		break;
+
+	case( 2 ):
+		tile1 = sl1;
+		tile2 = sl2;
+		tile3 = sl3;
+		tile4 = sl4;
+		modulo = 3;
+		break;
+	}
 
 	ttk_fillrect( srf, 0, 0, tglob.w, tglob.h, tglob.bg );
 	ttk_gc_set_foreground( gc, tglob.fg );
 
 	for( x=0 ; x<tglob.bw ; x++ ) for( y=0 ; y<tglob.bh ; y++ ) {
-		if( tglob.buf[x][y] & 1 ) {
-			t_GrBitmap( pixmap, gc, x*16, y*16, 16, 16, tile1 );
-		} else {
-			t_GrBitmap( pixmap, gc, x*16, y*16, 16, 16, tile2 );
+		unsigned short * tile = NULL;
+
+		switch( tglob.buf[x][y] & modulo ) {
+		case( 0 ): tile = tile1; break;
+		case( 1 ): tile = tile2; break;
+		case( 2 ): tile = tile3; break;
+		case( 3 ): tile = tile4; break;
+		default:   tile = tile1; break;
 		}
+
+		t_GrBitmap( pixmap, gc, x*16, y*16, 16, 16, tile );
 	}
 
 
@@ -230,7 +270,7 @@ static PzWindow *new_truchet_window_common( )
 				draw_truchet, event_truchet );
 
 	/* 1 second timeout */
-	pz_widget_set_timer( tglob.widget, 25 );
+	pz_widget_set_timer( tglob.widget, 1 );
 
 	/* we're waaaay more important than anyone else */
 	pz_set_priority(PZ_PRIORITY_VITAL); 
