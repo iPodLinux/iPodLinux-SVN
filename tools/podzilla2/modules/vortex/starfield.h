9323898b9fb1a5a5a932641b74b54e1a6562d406
bleullama <bleullama@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2006-04-18 20:40:21 +0000

    Added functionality to define where on the generation plane to generate the stars.

--- tools/podzilla2/modules/vortex/starfield.h
+++ tools/podzilla2/modules/vortex/starfield.h
@@ -15,6 +15,13 @@
 *
 ********************************************************************************************/
 
+#define Z_MAX_DIST 100
+#define STAR_MAX_VELOCITY 2
+
+#define MAX_INIT_STAR_X ttk_screen->w/2*Z_MAX_DIST
+#define MAX_INIT_STAR_Y ttk_screen->h/2*Z_MAX_DIST
+
+
 /* this registers the demo module */
 void Module_Starfield_init( void );
 
@@ -23,3 +30,15 @@ void Module_Starfield_session( int nstars, int velocity );
 
 /* this moves and draws the stars onto the surface */
 void Module_Starfield_draw( ttk_surface srf );
+
+/* this defines the region in which the stars will be generated */
+void Module_Starfield_genregion( int x_range, int x_offset,
+				 int y_range, int y_offset );
+
+/* default is:
+
+    Module_Starfield_genregion( MAX_INIT_STAR_X*2, -MAX_INIT_STAR_X,
+                                MAX_INIT_STAR_Y*2, -MAX_INIT_STAR_Y);
+
+*/
+
