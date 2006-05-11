44ff6a7a32222180582dcc1b6180363b220380da
courtc <ccavin@gmail.com>
2006-05-11 06:25:17 +0000

    first run of primitives, more to come; try 'make prim-demo'

--- tools/hotdog/hotdog_primitive.c
+++ tools/hotdog/hotdog_primitive.c
@@ -1,36 +1,40 @@
-/*** The stuff in this file needs to be updated! ***/
-
 /*
  * Copyright (c) 2005, James Jacobsson
+ * Copyright (c) 2006, Courtney Cavin
+ * Parts Copyright (c) A. Schiffler
  * All rights reserved.
  * 
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  * 
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or other
- * materials provided with the distribution.
- * Neither the name of the organization nor the names of its contributors may be used
- * to endorse or promote products derived from this software without specific prior
- * written permission.
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the organization nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
  * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
- * SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
- * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
- * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
+
 #include <stdio.h>
 #include <assert.h>
 
 #include "hotdog.h"
 
+#if 0
 void HD_Primitive_Render(hd_engine *eng,hd_object *obj, int cx, int cy, int cw, int ch) {
   int32 x,y;
   int32 startx,starty,endx,endy;
@@ -59,4 +63,271 @@ void HD_Primitive_Render(hd_engine *eng,hd_object *obj, int cx, int cy, int cw, 
   }
 #endif
 
+}
+#endif
+
+#define SWAP(a,b)	\
+  do {			\
+    (a) ^= (b);		\
+    (b) ^= (a);		\
+    (a) ^= (b);		\
+  } while (0)
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+#define ABS(a) (((a)>0)?(a):(-(a)))
+
+static void hLine(hd_surface srf, int x1, int x2, int y, uint32 col)
+{
+	uint32 *p;
+	/* TODO: check bounds */
+	p = HD_SRF_ROWF(srf, y);
+	for (; x1 < x2; ++x1)
+		BLEND_ARGB8888_ON_ARGB8888(*(p + x1), col, 0xff)
+}
+
+void HD_Pixel(hd_surface srf, int x, int y, uint32 col)
+{
+	HD_SRF_BLENDPIX(srf,x,y,col);
+}
+
+/* NOTE: last time I tried using this algorithm the line always was a pixel
+ * short. check for inconsistancies. */
+void HD_Line(hd_surface srf, int x0, int y0, int x1, int y1, uint32 col)
+{
+	char steep;
+	int dy, dx, x, y, yi;
+	int err = 0;
+
+	if (y0 == y1) {
+		hLine(srf, x0, x1, y1, col);
+		return;
+	}
+
+	steep = (ABS(y1 - y0) > ABS(x1 - x0));
+	if (steep) {
+		SWAP(x0, y0);
+		SWAP(x1, y1);
+	}
+	if (x0 > x1) {
+		SWAP(x0, x1);
+		SWAP(y0, y1);
+	}
+	dx = x1 - x0;
+	dy = ABS(y1 - y0);
+	y = y0;
+	yi = (y0 < y1) ? 1 : -1;
+	for (x = x0; x < x1; ++x) {
+		/* TODO: extract this if/else out of the loop */
+		if (steep) HD_SRF_BLENDPIX(srf,y,x,col);
+		else HD_SRF_BLENDPIX(srf,x,y,col);
+		err += dy;
+		if (2*err >= dx) {
+			y += yi;
+			err -= dx;
+		}
+	}
+#if 0 /* Bresenham's line algorithm */
+function line(x0, x1, y0, y1)
+     boolean steep := abs(y1 - y0) > abs(x1 - x0)
+     if steep then
+         swap(x0, y0)
+         swap(x1, y1)
+     if x0 > x1 then
+         swap(x0, x1)
+         swap(y0, y1)
+     int deltax := x1 - x0
+     int deltay := abs(y1 - y0)
+     int error := 0
+     int y := y0
+     if y0 < y1 then ystep := 1 else ystep := -1
+     for x from x0 to x1
+         if steep then plot(y,x) else plot(x,y)
+         error := error + deltay
+         if 2*error >= deltax
+             y := y + ystep
+             error := error - deltax
+#endif
+}
+
+void HD_FillRect(hd_surface srf, int x1, int y1, int x2, int y2, uint32 col)
+{
+	int xi;
+	uint32 *p;
+
+	if (x1 == x2) return;
+	if (x1 > x2) SWAP(x1, x2);
+	if (y1 == y2) return;
+	if (y1 > y2) SWAP(y1, y2);
+
+	y2 = MIN(HD_SRF_HEIGHT(srf), y2);
+	x2 = MIN(HD_SRF_WIDTH(srf), x2);
+
+	x1 = MAX(0, x1);
+	y1 = MAX(0, y1);
+
+	if (x2 <= x1 || y2 <= y1) return;
+
+	for (; y1 < y2; ++y1) {
+		/* TODO: increment rather than assign? */
+		p = HD_SRF_ROWF(srf, y1);
+		for (xi = x1; xi < x2; ++xi)
+			BLEND_ARGB8888_ON_ARGB8888(*(p + xi), col, 0xff)
+	}
+}
+
+/* XXX: resizes rectangle to surface; should clip and remove clipped sides */
+void HD_Rect(hd_surface srf, int x1, int y1, int x2, int y2, uint32 col)
+{
+	int xi;
+	uint32 *b, *e;
+
+	if (x1 == x2) return;
+	if (x1 > x2) SWAP(x1, x2);
+	if (y1 == y2) return;
+	if (y1 > y2) SWAP(y1, y2);
+
+	y2 = MIN(HD_SRF_HEIGHT(srf), y2);
+	x2 = MIN(HD_SRF_WIDTH(srf), x2);
+
+	x1 = MAX(0, x1);
+	y1 = MAX(0, y1);
+
+	if (x2 <= x1 || y2 <= y1) return;
+
+	b = HD_SRF_ROWF(srf, y1);
+	e = HD_SRF_ROWF(srf, y2 - 1);
+	for (xi = x1; xi < x2; ++xi) {
+		BLEND_ARGB8888_ON_ARGB8888(*(b + xi), col, 0xff)
+		BLEND_ARGB8888_ON_ARGB8888(*(e + xi), col, 0xff)
+	}
+	for (; y1 < y2; ++y1) {
+		b = HD_SRF_ROWF(srf, y1);
+		BLEND_ARGB8888_ON_ARGB8888(*(b + x1), col, 0xff)
+		BLEND_ARGB8888_ON_ARGB8888(*(b + x2 - 1), col, 0xff)
+	}
+}
+
+/* XXX: add clipping */
+void HD_Circle(hd_surface srf, int x, int y, int r, uint32 col)
+{
+	int16 cx = 0;
+	int16 cy = r;
+	int16 ocx = 0xffff;
+	int16 ocy = 0xffff;
+	int16 df = 1 - r;
+	int16 d_e = 3;
+	int16 d_se = -2 * r + 5;
+	int16 xpcx, xpcy, xmcx, xmcy;
+	int16 ypcx, ypcy, ymcx, ymcy;
+
+	if (r < 0) return;
+	if (r == 0) {
+		HD_SRF_BLENDPIX(srf, x, y, col);
+		return;
+	}
+
+	do {
+		if (ocy != cy || ocx != cx) {
+			xpcx = x + cx;
+			xmcx = x - cx;
+			if (cy > 0) {
+				ypcy = y + cy;
+				ymcy = y - cy;
+				HD_SRF_BLENDPIX(srf, xmcx, ypcy, col);
+				HD_SRF_BLENDPIX(srf, xpcx, ypcy, col);
+				HD_SRF_BLENDPIX(srf, xmcx, ymcy, col);
+				HD_SRF_BLENDPIX(srf, xpcx, ymcy, col);
+			}
+			else {
+				HD_SRF_BLENDPIX(srf, xmcx, y, col);
+				HD_SRF_BLENDPIX(srf, xpcx, y, col);
+			}
+			ocy = cy;
+			xpcy = x + cy;
+			xmcy = x - cy;
+			if (cx > 0) {
+				ypcx = y + cx;
+				ymcx = y - cx;
+				HD_SRF_BLENDPIX(srf, xmcy, ypcx, col);
+				HD_SRF_BLENDPIX(srf, xpcy, ypcx, col);
+				HD_SRF_BLENDPIX(srf, xmcy, ymcx, col);
+				HD_SRF_BLENDPIX(srf, xpcy, ymcx, col);
+			}
+			else {
+				HD_SRF_BLENDPIX(srf, xmcy, y, col);
+				HD_SRF_BLENDPIX(srf, xpcy, y, col);
+			}
+			ocx = cx;
+		}	
+		if (df < 0) {
+			df += d_e;
+			d_e += 2;
+			d_se += 2;
+		}
+		else {
+			df += d_se;
+			d_e += 2;
+			d_se += 4;
+			--cy;
+		}
+		++cx;
+	} while (cx <= cy);
+}
+
+/* XXX: add clipping */
+void HD_FillCircle(hd_surface srf, int x, int y, int r, uint32 col)
+{
+	int16 cx = 0;
+	int16 cy = r;
+	int16 ocx = 0xffff;
+	int16 ocy = 0xffff;
+	int16 df = 1 - r;
+	int16 d_e = 3;
+	int16 d_se = -2 * r + 5;
+	int16 xpcx, xpcy, xmcx, xmcy;
+
+	if (r < 0) return;
+	if (r == 0) {
+		HD_SRF_BLENDPIX(srf, x, y, col);
+		return;
+	}
+
+	do {
+		xpcx = x + cx;
+		xmcx = x - cx;
+		xpcy = x + cy;
+		xmcy = x - cy;
+		if (ocy != cy) {
+			if (cy > 0) {
+				hLine(srf, xmcx, xpcx, y + cy, col);
+				hLine(srf, xmcx, xpcx, y - cy, col);
+			}
+			else
+				hLine(srf, xmcx, xpcx, y, col);
+			ocy = cy;
+		}
+		if (ocx != cx) {
+			if (cx != cy) {
+				if (cx > 0) {
+					hLine(srf, xmcy, xpcy, y - cx, col);
+					hLine(srf, xmcy, xpcy, y + cx, col);
+				}
+				else
+					hLine(srf, xmcy, xpcy, y, col);
+			}
+			ocx = cx;
+		}	
+		if (df < 0) {
+			df += d_e;
+			d_e += 2;
+			d_se += 2;
+		}
+		else {
+			df += d_se;
+			d_e += 2;
+			d_se += 4;
+			--cy;
+		}
+		++cx;
+	} while (cx <= cy);
 }
---
+++ tools/hotdog/main_prim.c
@@ -0,0 +1,102 @@
+#include <stdlib.h>
+#include "hotdog.h"
+
+#ifdef IPOD
+uint32 *screen;
+
+static uint32 WIDTH, HEIGHT;
+#else
+#include "SDL.h"
+SDL_Surface *screen;
+
+#define WIDTH 220
+#define HEIGHT 176
+#endif
+
+hd_engine *engine;
+
+static void update(hd_engine *e, int x, int y, int w, int h)
+{
+#ifdef IPOD
+	HD_LCD_Update (eng->screen.framebuffer, 0, 0, WIDTH, HEIGHT);
+#else
+	SDL_UpdateRect(SDL_GetVideoSurface(), x, y, w, h);
+#endif
+}
+
+int main(int argc, char **argv)
+{
+	char eop = 0;
+	hd_surface srf;
+	hd_object *obj;
+#ifndef IPOD
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		fprintf(stderr,"Unable to init SDL: %s\n",SDL_GetError());
+		exit(1);
+	}
+	atexit(SDL_Quit);
+	screen = SDL_SetVideoMode(WIDTH, HEIGHT,16,SDL_SWSURFACE);
+	if (screen == NULL) {
+		fprintf(stderr,"Unable to init SDL video: %s\n",SDL_GetError());
+		exit(1);
+	}
+	engine = HD_Initialize (WIDTH, HEIGHT, 16, screen->pixels, update);
+#else
+	HD_LCD_Init();
+	HD_LCD_GetInfo(0, &WIDTH, &HEIGHT, 0);
+	screen = malloc(WIDTH * HEIGHT * 2);
+	engine = HD_Initialize(WIDTH, HEIGHT, 16, screen, update);
+#endif
+	obj = HD_New_Object();
+	obj->type = HD_TYPE_CANVAS;
+	obj->canvas = HD_NewSurface(WIDTH, HEIGHT);
+	obj->natw = obj->w = WIDTH;
+	obj->nath = obj->h = HEIGHT;
+	obj->render = HD_Canvas_Render;
+        obj->destroy = HD_Canvas_Destroy;
+	HD_Register(engine, obj);
+	srf = obj->canvas;
+	HD_Rect(srf, WIDTH/4, HEIGHT/3, WIDTH/2, HEIGHT/2, 0xff808080);
+	HD_Line(srf, 0, 0, WIDTH/2, HEIGHT/2, 0xffff0000);
+	HD_FillCircle(srf, WIDTH/4, HEIGHT/4, WIDTH/6, 0x80ff00ff);
+	HD_FillRect(srf, WIDTH/4, HEIGHT/4, WIDTH/2+WIDTH/4, HEIGHT/2+HEIGHT/4,
+			0x8000ff00);
+	HD_Circle(srf, WIDTH/2, HEIGHT/2, WIDTH/5, 0xff0000ff);
+
+	while (!eop) {
+#ifndef IPOD
+		SDL_Event e;
+		while (SDL_PollEvent(&e)) {
+			switch (e.type) {
+			case SDL_KEYDOWN:
+				switch (e.key.keysym.sym) {
+				case SDLK_ESCAPE:
+					eop = 1;
+					break;
+				default: break;
+				}
+				break;
+			case SDL_QUIT:
+				eop = 1;
+				break;
+			}
+		}
+		SDL_Delay(30);
+
+#endif
+#ifndef IPOD
+		if( SDL_MUSTLOCK(screen) )
+			SDL_LockSurface(screen);
+#endif
+
+		HD_Render(engine);
+
+#ifndef IPOD
+		if( SDL_MUSTLOCK(screen) )
+			SDL_UnlockSurface(screen);
+#endif
+		
+	}
+	return 0;
+	
+}
