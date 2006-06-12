54b86dae7e822856eb764b28aed360ab4e94317d
josh <josh@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2006-06-12 02:11:52 +0000

    Consolidate all the legacy games/demos into one module, `legacies'. Also ported Steroids + BlueCube.

---
+++ tools/podzilla2/modules/legacies/steroids/shot.h
@@ -0,0 +1,23 @@
+#ifndef __STEROIDS_SHOT_H__
+#define __STEROIDS_SHOT_H__
+
+#define PZ_COMPAT
+#include "pz.h"
+
+#include "object.h"
+#include "polygon.h"
+#include "ship.h"
+
+#define STEROIDS_SHOT_NUM     10
+#define STEROIDS_SHOT_FORCE    3
+#define STEROIDS_SHOT_CYCLES  30
+
+typedef struct SteroidsShot
+{
+    int             active;
+    long            cycles;    // The number of cycles the shot has been active
+    Steroids_Object shape;
+
+} Steroids_Shot;
+
+#endif
