29d7398ae5d4811951fa5fc7f3d41a6a6d305fde
courtc <courtc@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2006-08-13 02:45:48 +0000

    I uhh.. did that on purpose

--- tools/hotdog/hotdog_font.c
+++ tools/hotdog/hotdog_font.c
@@ -294,7 +294,7 @@ void HD_Font_Draw (hd_surface srf, hd_font *font, int x, int y, uint32 color, co
         HD_Font_DrawLatin1 (srf, font, x, y, color, str);
     else {
         unsigned short *buf = malloc (strlen (str) * 2 + 2);
-        unsigned int len = ConvertUTF8 ((unsigned const char *)str, buf);
+        unsigned int len = ConvertUTF8 ((const unsigned char *)str, buf);
         buf[len] = 0;
         HD_Font_DrawUnicode (srf, font, x, y, color, buf);
         free (buf);
@@ -307,7 +307,7 @@ void HD_Font_DrawFast (hd_surface srf, hd_font *font, int x, int y, uint32 color
         HD_Font_DrawFastLatin1 (srf, font, x, y, color, str);
     else {
         unsigned short *buf = malloc (strlen (str) * 2 + 2);
-        unsigned int len = ConvertUTF8 ((unsigned const char *)str, buf);
+        unsigned int len = ConvertUTF8 ((const unsigned char *)str, buf);
         buf[len] = 0;
         HD_Font_DrawFastUnicode (srf, font, x, y, color, buf);
         free (buf);
@@ -352,7 +352,7 @@ int HD_Font_TextWidth (hd_font *font, const char *str)
         return HD_Font_TextWidthLatin1 (font, str);
     else {
         unsigned short *buf = malloc (strlen (str) * 2 + 2);
-        unsigned int len = ConvertUTF8 ((unsigned const char *)str, buf);
+        unsigned int len = ConvertUTF8 ((const unsigned char *)str, buf);
         buf[len] = 0;
         int ret = HD_Font_TextWidthUnicode (font, buf);
         free (buf);
