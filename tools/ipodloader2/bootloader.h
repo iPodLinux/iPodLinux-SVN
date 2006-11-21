054de7743e752a940a90017fcd8daf77c5dd2c34
fxb <fxb@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2006-11-21 14:18:02 +0000

    Readability patch for Loader2. Thanks to Kasperle.

--- tools/ipodloader2/bootloader.h
+++ tools/ipodloader2/bootloader.h
@@ -20,5 +20,26 @@ typedef unsigned long size_t;
 #define inb(a) (*(volatile unsigned char *) (a))
 #define outb(a,b) (*(volatile unsigned char *) (b) = (a))
 
+typedef struct  {
+	uint8	status;
+	uint8	chs_start[3];
+	uint8	type;		/* filesystem type: e.g. 0x0a for FAT32, 0x83 for ext2fs */
+	uint8	chs_end[3];
+	uint32	lba_offset;	
+	uint32	lba_size;
+} __attribute__((__packed__)) pt_entry_t;
+
+typedef struct {
+	uint8	code[ 0x018a];		/* MBR Code */
+	uint8	ibm_ext_pte[36];	/* 4 9-byte primary partition table entries (some IBM stuff) */
+	uint8	unused[10];		/* unused */
+	uint32	disk_signature;		/* 4 byte disk signature */
+	uint16	:16;			/* unused */
+	pt_entry_t	partition_table[4];	/* the partition table */
+	uint16	MBR_signature;		/* the MBR signature */
+}  __attribute__((__packed__)) mbr_t;
+
 
 #endif
+
+
