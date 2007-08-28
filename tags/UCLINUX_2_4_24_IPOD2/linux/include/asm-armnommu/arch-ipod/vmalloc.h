cefddd8ae81b5e812a7b35b6b46bbcdb904dbc54
courtc <ccavin@gmail.com>
2007-08-28 07:34:07 +0000

    The Great Befuddlement

--- tags/UCLINUX_2_4_24_IPOD2/linux/include/asm-armnommu/arch-ipod/vmalloc.h
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2003, Bernard Leach (leachbj@bouncycastle.org)
- */
-
-#ifndef __ASM_ARCH_VMALLOC_H__
-#define __ASM_ARCH_VMALLOC_H__
-
-#endif
-
--- tags/linux_2_6_7_hsc0/linux-2.6/include/asm-armnommu/socket.h
+++
@@ -1,1 +0,0 @@
-#include <asm-arm/socket.h>
