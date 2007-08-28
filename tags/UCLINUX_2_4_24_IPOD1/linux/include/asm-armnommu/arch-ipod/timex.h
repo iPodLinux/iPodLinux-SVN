660943b37b8eccb6f7b8481aaa3fd3f2ef1c8e20
court <court@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2007-08-28 07:34:07 +0000

    The Great Befuddlement

--- tags/UCLINUX_2_4_24_IPOD1/linux/include/asm-armnommu/arch-ipod/timex.h
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2003, Bernard Leach (leachbj@bouncycastle.org)
- */
-
-#ifndef __ASM_ARCH_TIMEX_H__
-#define __ASM_ARCH_TIMEX_H__
-
-#endif
-
--- tags/linux_2_6_4_ipod0/linux-2.6/include/asm-armnommu/arch-ipod/param.h
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2003, Bernard Leach (leachbj@bouncycastle.org)
- */
-
-#ifndef __ARCH_ASM_PARAM_H__
-#define __ARCH_ASM_PARAM_H__
-
-#endif
-
