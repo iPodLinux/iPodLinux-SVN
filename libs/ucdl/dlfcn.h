41e5c829cfa5c0a069ee0ff81d40d0a51961d665
courtc <ccavin@gmail.com>
2007-09-22 03:49:30 +0000

    add incomplete dlfcn layer for ucdl

---
+++ libs/ucdl/dlfcn.h
@@ -0,0 +1,17 @@
+#ifndef _DLFCN_H_
+#define _DLFCN_H_
+
+void *dlopen(const char *filename, int flag);
+char *dlerror(void);
+void *dlsym(void *handle, const char *symbol);
+int dlclose(void *handle);
+
+#define RTLD_LAZY     1
+#define RTLD_NOW      2
+#define RTLD_GLOBAL   3
+#define RTLD_LOCAL    4
+#define RTLD_NODELETE 5
+#define RTLD_NOLOAD   6
+#define RTLD_DEEPBIND 7
+
+#endif
