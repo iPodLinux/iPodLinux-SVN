5070ea85e51e283f3ee8fbd7382a5ecae8b72616
courtc <ccavin@gmail.com>
2007-10-14 16:35:43 +0000

    added images and color drawing added resuming

--- apps/ipod/podzilla2/modules/othello/othello.c
+++ apps/ipod/podzilla2/modules/othello/othello.c
@@ -44,10 +44,14 @@ static signed char weights[] = {
 
 static PzModule *module;
 
-static State othello_state;
-static uint64_t board;
-static uint64_t b_set;
-static signed char cur_bit;
+static struct game_st {
+	State state;
+	uint64_t board;
+	uint64_t b_set;
+	signed char cur_bit, nocolor;
+	ttk_surface black, white, selection;
+} * _othello = 0;
+
 /* board is a 64 bit playing space
  * b_set is a 64 bit field of set pieces
  *
@@ -61,7 +65,7 @@ static signed char cur_bit;
 #define PC_PAD 3
 #define BO_YS ((wid->h - (PC_SZ+PC_PAD)*8)/2)
 #define BO_XS ((wid->w - (PC_SZ+PC_PAD)*8)/2)
-static void othello_draw(PzWidget *wid, ttk_surface srf)
+static void othello_mono_draw(PzWidget *wid, ttk_surface srf)
 {
 	unsigned char xline, yline, c;
 	int coordx, coordy;
@@ -75,9 +79,9 @@ static void othello_draw(PzWidget *wid, ttk_surface srf)
 		xline = c % 8;
 		coordx = xline*(PC_SZ+PC_PAD) + PC_PAD/2 + BO_XS;
 		coordy = yline*(PC_SZ+PC_PAD) + PC_PAD/2 + BO_YS;
-		if (b_set & (1ULL << c)) {
+		if (_othello->b_set & (1ULL << c)) {
 
-			if (board & (1ULL << c)) {
+			if (_othello->board & (1ULL << c)) {
 				ttk_aafillellipse(srf, coordx + PC_SZ/2,
 						coordy + PC_SZ/2, PC_SZ/2,
 						PC_SZ/2, fg);
@@ -92,7 +96,7 @@ static void othello_draw(PzWidget *wid, ttk_surface srf)
 			ttk_pixel(srf, coordx + PC_SZ/2,
 					coordy + PC_SZ/2, fg);
 		}
-		if (cur_bit == c) {
+		if (_othello->cur_bit == c) {
 			ttk_aaellipse(srf, coordx + PC_SZ/2,
 					coordy + PC_SZ/2, PC_SZ/2, PC_SZ/2,
 					ttk_ap_getx("scroll.bar")->color);
@@ -104,6 +108,51 @@ static void othello_draw(PzWidget *wid, ttk_surface srf)
 	}
 	/*XXX*/
 }
+
+void draw_image(ttk_surface img, ttk_surface srf, int x, int y)
+{
+	int w, h;
+	ttk_surface_get_dimen(img, &w, &h);
+	ttk_blit_image(img, srf, x - w/2, y - h/2);
+}
+
+#define BGCOLOR ttk_makecol(0x0d, 0x77, 0x0b)
+static void othello_color_draw(PzWidget *wid, ttk_surface srf)
+{
+	unsigned char xline, yline, c;
+	int x, y, step, startx, endx, size;
+
+#ifdef SDL
+	ttk_fillrect(srf, 0,0, wid->w, wid->h, ttk_ap_getx("window.bg")->color);
+#endif
+	step = (wid->h / 8);
+	size = step * 8;
+	startx = (wid->w - wid->h) / 2;
+	endx = startx + size;
+	ttk_fillrect(srf, startx,0, endx, size, BGCOLOR);
+	for (x = 0; x < 9; ++x) {
+		int mod = (size / 8) * x;
+		ttk_line(srf, startx, mod, endx, mod, 0x000000);
+		ttk_line(srf, startx + mod, 0, startx + mod, size, 0x000000);
+	}
+	yline = 0;
+	for (c = 0; c < 64; c++) {
+		xline = c % 8;
+		x = xline * step + step/2 + startx;
+		y = yline * step + step/2 + 0;
+		if (_othello->b_set & (1ULL << c)) {
+			if (_othello->board & (1ULL << c))
+				draw_image(_othello->black, srf, x, y);
+			else
+				draw_image(_othello->white, srf, x, y);
+		}
+		else if (_othello->cur_bit == c)
+			draw_image(_othello->selection, srf, x, y);
+		if (xline == 7) yline++;
+	}
+}
+
+
 
 static void set_piece(uint64_t *bo, uint64_t *bs, char side, unsigned char bit)
 {
@@ -259,12 +308,15 @@ static signed char best_move(uint64_t *bo, uint64_t *bs, char side)
 static int check_nomove()
 {
 	unsigned char c;
-	if (othello_state != NO_MOVE && othello_state != GAME_OVER) return 0;
+	if (_othello->state != NO_MOVE && _othello->state != GAME_OVER)
+		return 0;
 	for (c = 0; c < 64; c++) {
-		if (valid_move(&board, &b_set, S_WHITE, c)) return 0;
-		if (valid_move(&board, &b_set, S_BLACK, c)) return 0;
+		if (valid_move(&_othello->board, &_othello->b_set, S_WHITE, c))
+			return 0;
+		if (valid_move(&_othello->board, &_othello->b_set, S_BLACK, c))
+			return 0;
 	}
-	othello_state = GAME_OVER;
+	_othello->state = GAME_OVER;
 	return 1;
 }
 
@@ -279,18 +331,18 @@ static int computer_move(TWidget *this)
 	if (check_nomove()) return 0;
 	this->dirty = 1;
 
-	if ((c = best_move(&board, &b_set, S_WHITE)) != NOMOVE)
-		move_piece(&board, &b_set, S_WHITE, c);
+	if ((c = best_move(&_othello->board, &_othello->b_set, S_WHITE)) != NOMOVE)
+		move_piece(&_othello->board, &_othello->b_set, S_WHITE, c);
 	else
-		othello_state = NO_MOVE;
-	if (best_move(&board, &b_set, S_BLACK) == NOMOVE)
+		_othello->state = NO_MOVE;
+	if (best_move(&_othello->board, &_othello->b_set, S_BLACK) == NOMOVE)
 		return 0;
 
 	do {
-		cur_bit++;
-		WRAP(cur_bit, 0, 63);
-	} while (!valid_move(&board, &b_set, S_BLACK, cur_bit));
-	othello_state = WAITING;
+		_othello->cur_bit++;
+		WRAP(_othello->cur_bit, 0, 63);
+	} while (!valid_move(&_othello->board, &_othello->b_set, S_BLACK, _othello->cur_bit));
+	_othello->state = WAITING;
 	ttk_widget_set_timer(this, 0);
 	return 0;
 }
@@ -302,11 +354,11 @@ static int othello_event(PzEvent *e)
 	case PZ_EVENT_BUTTON_DOWN:
 		switch (e->arg) {
 		case PZ_BUTTON_ACTION:
-			if (check_nomove() || othello_state != WAITING) break;
-			if (move_piece(&board, &b_set, S_BLACK, cur_bit) !=
+			if (check_nomove() || _othello->state != WAITING) break;
+			if (move_piece(&_othello->board, &_othello->b_set, S_BLACK, _othello->cur_bit) !=
 					NOMOVE) {
 				ttk_widget_set_timer(e->wid, 1000); /* ms */
-				othello_state = THINKING;
+				_othello->state = THINKING;
 				e->wid->dirty = 1;
 			}
 			break;
@@ -319,12 +371,12 @@ static int othello_event(PzEvent *e)
 		}
 		break;
 	case PZ_EVENT_SCROLL:
-		if (check_nomove() || othello_state != WAITING) break;
+		if (check_nomove() || _othello->state != WAITING) break;
 		TTK_SCROLLMOD(e->arg, 5);
 		do {
-			cur_bit += e->arg;
-			WRAP(cur_bit, 0, 63);
-		} while (!valid_move(&board, &b_set, S_BLACK, cur_bit));
+			_othello->cur_bit += e->arg;
+			WRAP(_othello->cur_bit, 0, 63);
+		} while (!valid_move(&_othello->board, &_othello->b_set, S_BLACK, _othello->cur_bit));
 		e->wid->dirty = 1;
 		break;
 	default:
@@ -334,23 +386,74 @@ static int othello_event(PzEvent *e)
 	return ret;
 }
 
+#define safe_free(x) do { if (x) free(x), x = 0; } while (0)
+
+static void othello_destroy()
+{
+	if (!_othello || _othello->state != GAME_OVER)
+		return;
+
+	ttk_free_surface(_othello->black);
+	ttk_free_surface(_othello->white);
+	ttk_free_surface(_othello->selection);
+	safe_free(_othello);
+}
+
+static struct game_st *othello_create()
+{
+	char image[256];
+	const char *imgpath = pz_module_get_datapath(module, "images");
+	struct game_st *o = calloc(1, sizeof(struct game_st));
+
+	snprintf(image, 255, "%s/%d/black.png", imgpath, ttk_screen->h);
+	if (!(o->black = ttk_load_image(image)))
+		goto err;
+
+	snprintf(image, 255, "%s/%d/white.png", imgpath, ttk_screen->h);
+	if (!(o->white = ttk_load_image(image)))
+		goto err;
+
+	snprintf(image, 255, "%s/%d/selection.png", imgpath, ttk_screen->h);
+	if (!(o->selection = ttk_load_image(image)))
+		goto err;
+
+	return o;
+err:
+	fprintf(stderr, "Unable to load image `%s'\n", image);
+	o->nocolor = 1;
+	return o;
+}
+
 static PzWindow *new_othello_window()
 {
 	PzWindow *ret;
 	PzWidget *wid;
 
-	board = b_set = 0;
-	cur_bit = 19;
-	othello_state = WAITING;
+	if (_othello) {
+		if (pz_dialog(_("New Game?"), _("You have an unfinished game; "
+				"continue?"), 2, 0, _("Yes"), _("No"))) {
+			_othello->state = GAME_OVER;
+			othello_destroy();
+		}
+	}
+	if (!_othello) {
+		if (!(_othello = othello_create()))
+			return TTK_MENU_DONOTHING;
+		_othello->board = _othello->b_set = 0;
+		_othello->cur_bit = 19;
+		_othello->state = WAITING;
 
-	set_piece(&board, &b_set, S_WHITE, 27);
-	set_piece(&board, &b_set, S_WHITE, 36);
-	set_piece(&board, &b_set, S_BLACK, 28);
-	set_piece(&board, &b_set, S_BLACK, 35);
-
+		set_piece(&_othello->board, &_othello->b_set, S_WHITE, 27);
+		set_piece(&_othello->board, &_othello->b_set, S_WHITE, 36);
+		set_piece(&_othello->board, &_othello->b_set, S_BLACK, 28);
+		set_piece(&_othello->board, &_othello->b_set, S_BLACK, 35);
+	}
 	ret = pz_new_window(_("Othello"), PZ_WINDOW_NORMAL);
 
-	wid = pz_add_widget(ret, othello_draw, othello_event);
+	if (ttk_screen->bpp == 2 || _othello->nocolor)
+		wid = pz_add_widget(ret, othello_mono_draw, othello_event);
+	else
+		wid = pz_add_widget(ret, othello_color_draw, othello_event);
 
 	wid->dirty = 1;
 	wid->timer = computer_move;
