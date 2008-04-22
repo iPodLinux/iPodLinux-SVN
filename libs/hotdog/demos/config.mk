5198a53c62e63ffa67a28fc8894534806bd45d46
courtc <courtc@8e11cdb1-1bcf-4196-9f02-96aa8b954a99>
2008-04-22 01:21:40 +0000

    png depends on zlib, but it doesn't know that

--- libs/hotdog/demos/config.mk
+++ libs/hotdog/demos/config.mk
@@ -8,7 +8,7 @@ CFLAGS += -O2 -DIPOD -I$(HDDIR)/libs/libpng -I$(HDDIR)/libs/libjpeg -I$(HDDIR)/l
 LDFLAGS += -Wl,-elf2flt $(HDDIR)/libs/libpng/libpng.a $(HDDIR)/libs/libjpeg/libjpeg.a $(HDDIR)/libs/zlib/libz.a -L$(HDDIR)/ipod/ -lm
 else
 CFLAGS += -g `libpng-config --cflags` `sdl-config --cflags`
-LDFLAGS += `libpng-config --libs` `sdl-config --libs` -ljpeg -L$(HDDIR)/x11/ -lm
+LDFLAGS += `libpng-config --libs` `sdl-config --libs` -ljpeg -L$(HDDIR)/x11/ -lm -lz
 endif
 
 CC = $(CROSS)gcc
